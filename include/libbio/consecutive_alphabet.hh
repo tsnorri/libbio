1424ab9d06631c1dbe097eaef9422b0eebb5040f
tsnorri <tsnorri@iki.fi>
2018-08-16 20:07:35 +0300

    Make the use of the builder pattern more sensible
    – Remove the builder from the template parameters of the alphabet class.
    – Instead, use a base class for the builders. The base class has access to the alphabet's internals.

--- include/libbio/consecutive_alphabet.hh
+++ include/libbio/consecutive_alphabet.hh
@@ -73,79 +73,46 @@ namespace libbio { namespace detail {
 		template <typename t_builder, typename t_forward_iterable>
 		void prepare(t_builder &builder, t_forward_iterable const &text);
 	};
-	
-	
-	template <bool t_parallel>
-	template <typename t_iterator>
-	void consecutive_alphabet_as_parallel_builder_helper <t_parallel>::sort(t_iterator begin, t_iterator end)
-	{
-#if LIBBIO_HAVE_PARALLEL_STDLIB
-		std::sort(std::execution::par, m_found_characters.begin(), count + m_found_characters.begin());
-#endif
-	}
-	
-	
-	template <typename t_iterator>
-	void consecutive_alphabet_as_parallel_builder_helper <false>::sort(t_iterator begin, t_iterator end)
-	{
-		std::sort(begin, end);
-	}
-	
-	
-	template <bool t_parallel>
-	template <typename t_builder, typename t_forward_iterable>
-	void consecutive_alphabet_as_parallel_builder_helper <t_parallel>::prepare(t_builder &builder, t_forward_iterable const &text)
-	{
-#if LIBBIO_HAVE_PARALLEL_STDLIB
-		std::for_each(
-			std::execution::par_unseq,
-			text.cbegin(),
-			text.cend(),
-			[this](auto const c) {
-				if (0 == builder.m_flags[c].test_and_set())
-				{
-					// Mark the character as listed.
-					builder.m_found_characters[builder.m_found_char_idx++] = c;
-				}
-			}
-		);
-#endif
-	}
-	
-	
-	template <typename t_builder, typename t_forward_iterable>
-	void consecutive_alphabet_as_parallel_builder_helper <false>::prepare(t_builder &builder, t_forward_iterable const &text)
-	{
-		for (auto const c : text)
-		{
-			if (0 == builder.m_flags[c].test_and_set())
-			{
-				// Mark the character as listed.
-				builder.m_found_characters[builder.m_found_char_idx++] = c;
-			}
-		}
-	}
 }}
 
 
 namespace libbio {
 	
+	template <typename t_alphabet>
+	class consecutive_alphabet_mt_builder
+	{
+	protected:
+		typedef typename t_alphabet::map_type map_type;
+		
+	protected:
+		t_alphabet					m_alphabet;
+		
+	public:
+		t_alphabet &alphabet() { return m_alphabet; }
+		t_alphabet const &alphabet() const { return m_alphabet; }
+		
+	protected:
+		map_type &to_comp() { return m_alphabet.m_to_comp; }
+		map_type &to_char() { return m_alphabet.m_to_char; }
+	};
+	
+	
 	// Map characters to consecutive integers using the given map type.
-	template <
-		typename t_char,
-		template <typename> typename t_map,
-		typename t_builder = void
-	>
+	template <typename t_char, template <typename> typename t_map>
 	class consecutive_alphabet_mt
 	{
-		friend t_builder;
+		template <typename>
+		friend class consecutive_alphabet_mt_builder;
 		
 	public:
 		enum { ALPHABET_MAX = std::numeric_limits <t_char>::max() };
 		
 	protected:
-		t_map <t_char>			m_to_comp;
-		t_map <t_char>			m_to_char;
+		typedef t_map <t_char>	map_type;
+		
+	protected:
+		map_type				m_to_comp;
+		map_type				m_to_char;
 		
 	public:
 		consecutive_alphabet_mt() = default;
@@ -154,46 +121,29 @@ namespace libbio {
 		inline t_char comp_to_char(t_char const c) const { return m_to_char[c]; }
 		
 		// For debugging.
-		void print()
-		{
-			for (std::size_t i(0), count(m_to_char.size()); i < count; ++i)
-			{
-				t_char const c(m_to_char[i]);
-				std::cerr << "[" << i << "]: '" << c << "' ("
-				<< std::hex << +c << std::dec << ")\n";
-
-#ifndef NDEBUG
-				auto const comp(m_to_comp[c]);
-				assert(comp == i);
-#endif
-			}
-		}
+		void print();
+		
+		void swap(consecutive_alphabet_mt &other);
 	};
 	
 	
+	// Map with std::arrays.
 	template <typename t_char, typename t_builder = void>
-	using consecutive_alphabet_as = consecutive_alphabet_mt <
-		t_char,
-		detail::consecutive_alphabet_as_helper::map_type,
-		t_builder
-	>;
+	using consecutive_alphabet_as = consecutive_alphabet_mt <t_char, detail::consecutive_alphabet_as_helper::map_type>;
 	
+	// Map with std::maps.
 	template <typename t_char, typename t_builder = void>
-	using consecutive_alphabet_map = consecutive_alphabet_mt <
-		t_char,
-		detail::consecutive_alphabet_map_helper::map_type,
-		t_builder
-	>;
+	using consecutive_alphabet_map = consecutive_alphabet_mt <t_char, detail::consecutive_alphabet_map_helper::map_type>;
 	
 	
+	// Create a compressed alphabet.
 	template <typename t_char>
-	class consecutive_alphabet_as_builder
+	class consecutive_alphabet_as_builder : public consecutive_alphabet_mt_builder <consecutive_alphabet_as <t_char>>
 	{
-	public:
-		typedef consecutive_alphabet_as <t_char, consecutive_alphabet_as_builder>				alphabet_type;
+	protected:
+		typedef consecutive_alphabet_as <t_char>	alphabet_type;
 		
 	protected:
-		alphabet_type					m_alphabet;
 		std::vector <bool>				m_seen;
 		std::vector <t_char>			m_found_characters;
 		
@@ -206,25 +156,21 @@ namespace libbio {
 		void prepare(t_forward_iterable const &text);
 		
 		void compress();
-		alphabet_type &alphabet() { return m_alphabet; }
-		alphabet_type const &alphabet() const { return m_alphabet; }
 	};
 	
 	
+	// Create a compressed alphabet, parallelize where possible.
 	template <typename t_char>
-	class consecutive_alphabet_as_parallel_builder
+	class consecutive_alphabet_as_parallel_builder : public consecutive_alphabet_mt_builder <consecutive_alphabet_as <t_char>>
 	{
 		template <bool>
 		friend struct detail::consecutive_alphabet_as_parallel_builder_helper;
 		
 	protected:
-		typedef std::atomic <smallest_unsigned_lockfree_type_gte_t <t_char>>				counter_type;
-		
-	public:
-		typedef consecutive_alphabet_as <t_char, consecutive_alphabet_as_parallel_builder>	alphabet_type;
+		typedef consecutive_alphabet_as <t_char>								alphabet_type;
+		typedef std::atomic <smallest_unsigned_lockfree_type_gte_t <t_char>>	counter_type;
 		
 	protected:
-		alphabet_type					m_alphabet;
 		std::vector <std::atomic_flag>	m_flags;
 		std::vector <t_char>			m_found_characters;
 		counter_type					m_found_char_idx{1}; // Consider the NUL character.
@@ -242,9 +188,48 @@ namespace libbio {
 		template <bool t_parallel = true>
 		void compress();
 		
-		alphabet_type &alphabet() { return m_alphabet; }
-		alphabet_type const &alphabet() const { return m_alphabet; }
+		// Non-template variant.
+		void compress() { compress <true>(); }
 	};
+}
+
+
+namespace libbio {
+	
+	template <typename t_char, template <typename> typename t_map>
+	void swap(
+		consecutive_alphabet_mt <t_char, t_map> &lhs,
+		consecutive_alphabet_mt <t_char, t_map> &rhs
+	)
+	{
+		lhs.swap(rhs);
+	}
+	
+	
+	template <typename t_char, template <typename> typename t_map>
+	void consecutive_alphabet_mt <t_char, t_map>::swap(consecutive_alphabet_mt &other)
+	{
+		using std::swap;
+		swap(m_to_comp, other.m_to_comp);
+		swap(m_to_char, other.m_to_char);
+	}
+	
+	
+	template <typename t_char, template <typename> typename t_map>
+	void consecutive_alphabet_mt <t_char, t_map>::print()
+	{
+		for (std::size_t i(0), count(m_to_char.size()); i < count; ++i)
+		{
+			t_char const c(m_to_char[i]);
+			std::cerr << "[" << i << "]: '" << c << "' ("
+			<< std::hex << +c << std::dec << ")\n";
+
+#ifndef NDEBUG
+			auto const comp(m_to_comp[c]);
+			assert(comp == i);
+#endif
+		}
+	}
 	
 	
 	template <typename t_char>
@@ -300,13 +285,16 @@ namespace libbio {
 		// Use O(σ log σ) time to sort the characters.
 		std::sort(m_found_characters.begin(), m_found_characters.end());
 		
+		auto &to_comp(this->to_comp());
+		auto &to_char(this->to_char());
+		
 		std::size_t char_idx(0);
 		for (auto const c : m_found_characters)
 		{
 			assert(1 == m_seen[c]);
 			
-			m_alphabet.m_to_comp[c] = char_idx;
-			m_alphabet.m_to_char[char_idx] = c;
+			to_comp[c] = char_idx;
+			to_char[char_idx] = c;
 			++char_idx;
 		}
 	}
@@ -324,19 +312,74 @@ namespace libbio {
 		detail::consecutive_alphabet_as_parallel_builder_helper <t_parallel && LIBBIO_HAVE_PARALLEL_STDLIB> helper;
 		helper.sort(m_found_characters.begin(), count + m_found_characters.begin());
 		
-		m_alphabet.m_to_char.resize(count);
-		m_alphabet.m_to_comp.resize(1 + alphabet_type::ALPHABET_MAX);
+		auto &to_comp(this->to_comp());
+		auto &to_char(this->to_char());
 		
 		for (std::size_t i(0); i < count; ++i)
 		{
 			auto const c(m_found_characters[i]);
-			assert(m_flags[c]);
-			m_alphabet.m_to_comp[c] = char_idx;
-			m_alphabet.m_to_char[char_idx] = c;
+			assert(m_flags[c].test_and_set());
+			to_comp[c] = char_idx;
+			to_char[char_idx] = c;
 			++char_idx;
 		}
 	}
 }
+
+
+namespace libbio { namespace detail {
+	
+	template <bool t_parallel>
+	template <typename t_iterator>
+	void consecutive_alphabet_as_parallel_builder_helper <t_parallel>::sort(t_iterator begin, t_iterator end)
+	{
+#if LIBBIO_HAVE_PARALLEL_STDLIB
+		std::sort(std::execution::par, m_found_characters.begin(), count + m_found_characters.begin());
+#endif
+	}
+	
+	
+	template <typename t_iterator>
+	void consecutive_alphabet_as_parallel_builder_helper <false>::sort(t_iterator begin, t_iterator end)
+	{
+		std::sort(begin, end);
+	}
+	
+	
+	template <bool t_parallel>
+	template <typename t_builder, typename t_forward_iterable>
+	void consecutive_alphabet_as_parallel_builder_helper <t_parallel>::prepare(t_builder &builder, t_forward_iterable const &text)
+	{
+#if LIBBIO_HAVE_PARALLEL_STDLIB
+		std::for_each(
+			std::execution::par_unseq,
+			text.cbegin(),
+			text.cend(),
+			[this](auto const c) {
+				if (0 == builder.m_flags[c].test_and_set())
+				{
+					// Mark the character as listed.
+					builder.m_found_characters[builder.m_found_char_idx++] = c;
+				}
+			}
+		);
+#endif
+	}
+	
+	
+	template <typename t_builder, typename t_forward_iterable>
+	void consecutive_alphabet_as_parallel_builder_helper <false>::prepare(t_builder &builder, t_forward_iterable const &text)
+	{
+		for (auto const c : text)
+		{
+			if (0 == builder.m_flags[c].test_and_set())
+			{
+				// Mark the character as listed.
+				builder.m_found_characters[builder.m_found_char_idx++] = c;
+			}
+		}
+	}
+}}
 
 
 #endif
