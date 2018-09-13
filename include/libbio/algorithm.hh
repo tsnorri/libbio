72f12fdf72b9a751cde71069478e3b2402160d8c
Tuukka Norri <tsnorri@iki.fi>
2018-09-13 13:17:18 +0300

    Add unique_count

--- include/libbio/algorithm.hh
+++ include/libbio/algorithm.hh
@@ -7,6 +7,19 @@
 #define LIBBIO_ALGORITHM_HH
 
 
+namespace libbio { namespace detail {
+	
+	struct default_increment
+	{
+		template <typename t_item>
+		void operator()(t_item &item)
+		{
+			++item.count;
+		}
+	};
+}}
+
+
 namespace libbio {
 	
 	template <typename t_type_1, typename t_type_2>
@@ -69,6 +82,50 @@ namespace libbio {
 		
 		retval += std::distance(first_2, last_2);
 		return retval;
+	}
+	
+	
+	template <typename t_iterator, typename t_dst, typename t_increment_count>
+	void unique_count(t_iterator &&begin, t_iterator const &&end, t_dst &dst, t_increment_count &&increment)
+	{
+		dst.clear();
+		while (true)
+		{
+			auto it(std::adjacent_find(begin, end));
+			if (it == end)
+				break;
+			
+			// Copy the previous range to the buffer.
+			std::copy(begin, it, std::back_inserter(dst));
+			
+			// Determine the size of the run. We already determined that there is one copy.
+			auto val(*it); // Copy.
+			increment(val);
+			assert(it + 1 != end);
+			auto next(it + 2);
+			while (next != end && val == *next)
+			{
+				increment(val);
+				++next;
+			}
+			
+			// Copy the first element of the range.
+			dst.emplace_back(std::move(val));
+			
+			// Either next == end or *next is the first non-equivalent pair.
+			begin = next;
+		}
+		
+		// Copy the last range.
+		std::copy(begin, end, std::back_inserter(dst));
+	}
+	
+	
+	template <typename t_iterator, typename t_dst>
+	void unique_count(t_iterator &&begin, t_iterator const &&end, t_dst &dst)
+	{
+		detail::default_increment inc;
+		unique_count(std::forward <t_iterator>(begin), std::forward <t_iterator const>(end), dst, inc);
 	}
 }
 
---
+++ tests/algorithm.cc
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2018 Tuukka Norri
+ * This code is licensed under MIT license (see LICENSE for details).
+ */
+
+#include <catch2/catch.hpp>
+#include <cstdint>
+#include <libbio/algorithm.hh>
+#include <ostream>
+#include <vector>
+
+namespace gen	= Catch::Generators;
+namespace lb	= libbio;
+
+
+namespace {
+	
+	struct unique_count_test_input
+	{
+		std::uint32_t	value{};
+		std::uint32_t	count{};
+		
+		unique_count_test_input() = default;
+		unique_count_test_input(std::uint32_t value_, std::uint32_t count_):
+			value(value_),
+			count(count_)
+		{
+		}
+		
+		bool operator==(unique_count_test_input const &other) const
+		{
+			return value == other.value;
+		}
+	};
+	
+	std::ostream &operator<<(std::ostream &os, unique_count_test_input const &input)
+	{
+		os << '(' << input.value << ", " << input.count << ')';
+		return os;
+	}
+}
+
+
+SCENARIO("Set symmetric difference can be determined")
+{
+	GIVEN("Two collections")
+	{
+		auto const vectors = GENERATE(gen::table({
+			std::make_tuple(5, std::vector <unsigned int>({1, 3, 5, 81}), std::vector <unsigned int>({2, 3, 4, 5, 6})),
+			std::make_tuple(0, std::vector <unsigned int>({2, 4, 6, 8}), std::vector <unsigned int>({2, 4, 6, 8})),
+			std::make_tuple(0, std::vector <unsigned int>({1, 2, 3}), std::vector <unsigned int>({1, 2, 3})),
+			std::make_tuple(6, std::vector <unsigned int>({1, 2, 3}), std::vector <unsigned int>({4, 5, 6})),
+			std::make_tuple(6, std::vector <unsigned int>({1, 3, 5}), std::vector <unsigned int>({2, 4, 6}))
+		}));
+
+		
+		WHEN("the collections are compared")
+		{
+			auto const &tup(vectors);
+			auto const expected_count(std::get <0>(tup));
+			auto const &lhs(std::get <1>(tup));
+			auto const &rhs(std::get <2>(tup));
+			
+			auto const actual_count(lb::set_symmetric_difference_size(lhs.cbegin(), lhs.cend(), rhs.cbegin(), rhs.cend()));
+
+			THEN("the difference size matches the expected")
+			{
+				REQUIRE(expected_count == actual_count);
+			}
+		}
+	}
+}
+
+
+SCENARIO("Unique items can be counted")
+{
+	GIVEN("A collection of items")
+	{
+		typedef unique_count_test_input ti;
+		
+		auto const vectors = GENERATE(gen::table({
+			std::make_tuple(std::vector <ti>({{1, 1}, {2, 1}, {2, 1}, {4, 1}, {4, 1}, {4, 1}, {5, 1}}), std::vector <ti>({{1, 1}, {2, 2}, {4, 3}, {5, 1}})),
+			std::make_tuple(std::vector <ti>({{1, 1}, {2, 1}, {4, 1}}), std::vector <ti>({{1, 1}, {2, 1}, {4, 1}})),
+			std::make_tuple(std::vector <ti>({{3, 1}, {3, 1}, {3, 1}}), std::vector <ti>({{3, 3}}))
+		}));
+		
+		WHEN("the unique items are counted")
+		{
+			auto const &tup(vectors);
+			auto &input(std::get <0>(tup));
+			auto const &expected(std::get <1>(tup));
+			std::vector <ti> output;
+			
+			lb::unique_count(input.begin(), input.end(), output);
+			
+			THEN("the output matches the expected")
+			{
+				REQUIRE(expected.size() == output.size());
+				for (std::size_t i(0), count(expected.size()); i < count; ++i)
+				{
+					auto const &output_val(output[i]);
+					auto const &expected_val(expected[i]);
+					REQUIRE(output_val == expected_val);
+				}
+			}
+		}
+	}
+}
