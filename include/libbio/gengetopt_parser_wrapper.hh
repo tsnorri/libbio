700651c5ef016aa8a8d24ac1f65c649cddd46d9d
tsnorri <tsnorri@iki.fi>
2018-10-01 22:33:14 +0300

    Swap when moving
    – This should prevent dangling pointers.

--- include/libbio/gengetopt_parser_wrapper.hh
+++ include/libbio/gengetopt_parser_wrapper.hh
@@ -13,26 +13,20 @@ namespace libbio {
 	class gengetopt_parser_wrapper
 	{
 	public:
-		gengetopt_args_info	m_args;
+		gengetopt_args_info	m_args{};
 		
 	public:
 		gengetopt_parser_wrapper() = default;
-		inline ~gengetopt_parser_wrapper();
+		inline ~gengetopt_parser_wrapper() { cmdline_parser_free(&m_args); }
 		gengetopt_parser_wrapper(gengetopt_parser_wrapper const &) = delete;
-		gengetopt_parser_wrapper(gengetopt_parser_wrapper &&) = default;		
+		gengetopt_parser_wrapper(gengetopt_parser_wrapper &&other) { using std::swap; swap(m_args, other.m_args); }
 		gengetopt_parser_wrapper &operator=(gengetopt_parser_wrapper const &) & = delete;
-		gengetopt_parser_wrapper &operator=(gengetopt_parser_wrapper &&) & = default;
+		gengetopt_parser_wrapper &operator=(gengetopt_parser_wrapper &&other) & { using std::swap; swap(m_args, other.m_args); return *this; }
 		
 		inline void parse(int argc, char **argv);
 		inline gengetopt_args_info &args() { return m_args; }
 		inline gengetopt_args_info const &args() const { return m_args; }
 	};
-	
-	
-	gengetopt_parser_wrapper::~gengetopt_parser_wrapper()
-	{
-		cmdline_parser_free(&m_args);
-	}
 	
 	
 	void gengetopt_parser_wrapper::parse(int argc, char **argv)
