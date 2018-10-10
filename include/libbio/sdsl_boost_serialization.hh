2c0eaa3cd222908281fcccdc10ac4d097aa06adf
tsnorri <tsnorri@iki.fi>
2018-10-10 21:03:06 +0300

    Fix serializing the capacity, const correctness

--- include/libbio/sdsl_boost_serialization.hh
+++ include/libbio/sdsl_boost_serialization.hh
@@ -18,40 +18,37 @@ namespace boost { namespace serialization {
 	template <typename t_archive, std::uint8_t t_width>
 	void save(t_archive &ar, sdsl::int_vector <t_width> const &vector, unsigned int const version)
 	{
-		std::uint64_t const bit_capacity(vector.bit_capacity());
+		std::uint64_t capacity(vector.capacity());
 		std::uint64_t const bit_size(vector.bit_size());
 		auto const *data(vector.data());
 		
-		// Check that the capacity is a multiple of 64.
-		libbio_always_assert(0 == bit_capacity % 64);
-		
-		ar & bit_capacity;
+		ar & capacity;
 		ar & bit_size;
-		ar & make_binary_object(data, bit_capacity / CHAR_BIT); // binary_object stores a sequence of bytes.
+		ar & make_binary_object(data, vector.bit_capacity() / CHAR_BIT); // binary_object stores a sequence of bytes.
 	}
 	
 	
 	template <typename t_archive, std::uint8_t t_width>
 	void load(t_archive &ar, sdsl::int_vector <t_width> &vector, unsigned int const version)
 	{
-		std::uint64_t bit_capacity(0);
+		std::uint64_t capacity(0);
 		std::uint64_t bit_size(0);
 		binary_object obj(nullptr, 0); // No default constructor.
 		
-		ar & bit_capacity;
+		ar & capacity;
 		ar & bit_size;
 		ar & obj;
 		
-		libbio_always_assert(bit_capacity / CHAR_BIT == obj.m_size);
+		libbio_always_assert(vector.bit_capacity() / CHAR_BIT == obj.m_size);
 		
-		vector.bit_reserve(bit_capacity);
+		vector.reserve(capacity);
 		vector.bit_resize(bit_size);
 		
 		// binary_object stores the data in a void *. Play safe and assume that
 		// the data is byte-aligned.
 		auto *dst(vector.data());
 		auto *src(obj.m_t);
-		std::copy_n(reinterpret_cast <char const *>(src), obj.m_size, reinterpret_cast <char const *>(dst));
+		std::copy_n(reinterpret_cast <char const *>(src), obj.m_size, reinterpret_cast <char *>(dst));
 	}
 	
 	
