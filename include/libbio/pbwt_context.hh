ed40cfc61d1864b48b33ca98b6a5d18dac4415c7
tsnorri <tsnorri@iki.fi>
2018-10-11 15:30:34 +0300

    Remove buffering_pbwt_context because it caused more problems than it was worth

--- include/libbio/pbwt_context.hh
+++ include/libbio/pbwt_context.hh
@@ -15,66 +15,6 @@
 
 namespace libbio { namespace pbwt { namespace detail {
 	
-	template <typename t_buffering_context>
-	class buffering_context_callback_proxy
-	{
-	public:
-		typedef t_buffering_context								context_type;
-		typedef typename context_type::string_index_vector		string_index_vector;
-		typedef typename context_type::character_index_vector	character_index_vector;
-		typedef typename context_type::divergence_count_list	divergence_count_list;
-		
-	protected:
-		t_buffering_context	*m_context{};
-		std::size_t			m_buffer_idx{};
-		
-	public:
-		buffering_context_callback_proxy() = default;
-		buffering_context_callback_proxy(t_buffering_context &context, std::size_t buffer_idx):
-			m_context(&context),
-			m_buffer_idx(buffer_idx)
-		{
-		}
-		
-		string_index_vector const &output_permutation() const { return m_context->m_permutations.column(m_buffer_idx); };
-		character_index_vector const &output_divergence() const { return m_context->m_divergences.column(m_buffer_idx); };
-		divergence_count_list const &output_divergence_value_counts() const { return m_context->m_divergence_value_counts[m_buffer_idx]; };
-	};
-	
-	
-	template <bool t_callback_type>
-	struct callback_function_helper
-	{
-		template <typename t_context, typename t_fn>
-		static inline void call(t_context &context, t_fn &&fn) { fn(context.sequence_idx()); }
-	};
-	
-	template <>
-	struct callback_function_helper <true>
-	{
-		template <typename t_context, typename t_fn>
-		static inline void call(t_context &context, t_fn &&fn) { fn(context.sequence_idx(), context); }
-	};
-	
-	template <bool t_use_semaphore>
-	struct callback_function_helper_bc
-	{
-		template <typename t_context, typename t_fn>
-		static inline void call(t_context &context, t_fn &&fn, std::size_t) { fn(context.sequence_idx()); }
-	};
-	
-	template <>
-	struct callback_function_helper_bc <true>
-	{
-		template <typename t_context, typename t_fn>
-		static inline void call(t_context &context, t_fn &&fn, std::size_t buffer_idx)
-		{
-			buffering_context_callback_proxy proxy(context, buffer_idx);
-			fn(context.sequence_idx(), proxy);
-		}
-	};
-	
-	
 	// Adapt RMQ support’s constructor.
 	// Assume SDSL’s constructor parameters.
 	template <typename t_rmq>
@@ -147,7 +87,7 @@ namespace libbio { namespace pbwt {
 		DIVERGENCE_VALUE_COUNTS		= 0x80,
 		ALL							= 0xff
 	};
-
+	
 #	define LIBBIO_PBWT_CONTEXT_TEMPLATE_ARGS \
 	typename t_sequence_vector, \
 	typename t_alphabet_type, \
@@ -171,49 +111,11 @@ namespace libbio { namespace pbwt {
 		t_count_vector, \
 		t_divergence_count_type \
 	>
-				
-#	define LIBBIO_PBWT_BUFFERING_PBWT_CONTEXT_TEMPLATE_DECL \
-	template < \
-		typename t_sequence_vector, \
-		typename t_alphabet_type, \
-		typename t_ci_rmq, \
-		typename t_string_index, \
-		typename t_character_index, \
-		typename t_character_count, \
-		typename t_divergence_count, \
-		template <typename> typename t_vector_tpl \
-	>
-#	define LIBBIO_PBWT_BUFFERING_PBWT_CONTEXT_CLASS_DECL buffering_pbwt_context < \
-		t_sequence_vector, \
-		t_alphabet_type, \
-		t_ci_rmq, \
-		t_string_index, \
-		t_character_index, \
-		t_character_count, \
-		t_divergence_count, \
-		t_vector_tpl \
-	>
-	
-	// Forward declaration.
-	LIBBIO_PBWT_BUFFERING_PBWT_CONTEXT_TEMPLATE_DECL
-	class buffering_pbwt_context;
 	
 	
 	LIBBIO_PBWT_CONTEXT_TEMPLATE_DECL
 	class pbwt_context
 	{
-		template <
-			typename,
-			typename,
-			typename,
-			typename,
-			typename,
-			typename,
-			typename,
-			template <typename> typename
-		>
-		friend class buffering_pbwt_context;
-		
 	public:
 		typedef t_string_index_vector						string_index_vector;
 		typedef typename string_index_vector::value_type	string_index_type;
@@ -299,37 +201,16 @@ namespace libbio { namespace pbwt {
 		void copy_fields_in_use(pbwt_context const &other);
 		void clear_unused_fields();
 		
-		void release_lock() const {} // No-op, for compatibility with buffering_pbwt_context.
-		
 		template <
-			context_field t_extra_fields,
+			context_field t_extra_fields = context_field::NONE,
 			typename t_callback_fn
 		>
 		void process(
 			t_callback_fn &&callback_fn
-		) { process <false, t_extra_fields>(SIZE_MAX, callback_fn); }
+		) { process <t_extra_fields>(SIZE_MAX, callback_fn); }
 		
 		template <
-			context_field t_extra_fields,
-			typename t_callback_fn
-		>
-		void process(
-			std::size_t const caller_limit,
-			t_callback_fn &&callback_fn
-		) { process <false, t_extra_fields>(caller_limit, callback_fn); }
-		
-		template <
-			bool t_dummy,	// For compatibility with the buffering variant.
-			context_field t_extra_fields,
-			typename t_callback_fn
-		>
-		void process(
-			t_callback_fn &&callback_fn
-		) { process <t_dummy, t_extra_fields>(SIZE_MAX, callback_fn); }
-			
-		template <
-			bool t_dummy,	// For compatibility with the buffering variant.
-			context_field t_extra_fields,
+			context_field t_extra_fields = context_field::NONE,
 			typename t_callback_fn
 		>
 		void process(
@@ -339,7 +220,7 @@ namespace libbio { namespace pbwt {
 		
 		std::size_t unique_substring_count_lhs(std::size_t const lb) const { return libbio::pbwt::unique_substring_count(lb, m_input_divergence); }
 		std::size_t unique_substring_count_rhs(std::size_t const lb) const { return libbio::pbwt::unique_substring_count(lb, m_output_divergence); }
-
+		
 		template <typename t_counts>
 		std::size_t unique_substring_count_lhs(std::size_t const lb, t_counts &counts) const { return libbio::pbwt::unique_substring_count(lb, m_input_divergence, counts); }
 		
@@ -365,142 +246,6 @@ namespace libbio { namespace pbwt {
 	};
 	
 	
-	
-	// Buffer for multiple PBWT arrays. Intended to be used in a producer thread.
-	LIBBIO_PBWT_BUFFERING_PBWT_CONTEXT_TEMPLATE_DECL
-	class buffering_pbwt_context
-	{
-		template <typename>
-		friend class detail::buffering_context_callback_proxy;
-		
-	protected:
-		typedef libbio::detail::array_list_item <t_divergence_count>	divergence_count_item;
-		
-	public:
-		typedef t_sequence_vector										sequence_vector;
-		typedef t_alphabet_type											alphabet_type;
-		typedef t_ci_rmq												character_index_vector_rmq;
-		typedef matrix <t_string_index>									string_index_matrix;
-		typedef matrix <t_character_index>								character_index_matrix;
-		typedef array_list <t_divergence_count>							divergence_count_list;
-		typedef std::vector <divergence_count_list>						divergence_count_matrix;
-		typedef t_vector_tpl <t_character_count>						count_vector;
-		typedef t_vector_tpl <t_string_index>							string_index_vector;
-		typedef typename character_index_matrix::const_slice_type		character_index_vector;
-		
-		typedef pbwt_context <
-			sequence_vector,
-			alphabet_type,
-			character_index_vector_rmq,
-			t_vector_tpl <t_string_index>,
-			t_vector_tpl <t_character_index>,
-			count_vector,
-			t_divergence_count
-		> sample_context_type;
-		
-		typedef std::vector <sample_context_type>					sample_vector;
-	
-	protected:
-		sequence_vector const										*m_sequences{};
-		alphabet_type const											*m_alphabet{};
-		libbio::dispatch_ptr <dispatch_semaphore_t>					m_process_sema{};
-		std::uint64_t												m_sample_rate{std::numeric_limits <std::uint64_t>::max()};
-		std::size_t													m_buffer_count{};
-		std::size_t													m_sequence_idx{};
-		context_field												m_fields_in_use{
-																		context_field::ALL &
-																		~context_field::INVERSE_INPUT_PERMUTATION &
-																		~context_field::DIVERGENCE_VALUE_COUNTS
-																	};
-		
-		// FIXME: add inverse input permutation?
-		string_index_matrix											m_permutations;
-		character_index_matrix										m_divergences;
-		divergence_count_matrix										m_divergence_value_counts;
-		count_vector												m_character_counts;
-		string_index_vector											m_previous_positions;
-		sample_vector												m_samples;
-	
-	public:
-		buffering_pbwt_context() = default;
-	
-		buffering_pbwt_context(
-			std::size_t const buffer_count,
-			sequence_vector const &sequences,
-			alphabet_type const &alphabet,
-			context_field const extra_fields = context_field::NONE
-		):
-			m_sequences(&sequences),
-			m_alphabet(&alphabet),
-			m_process_sema(dispatch_semaphore_create(buffer_count)),
-			m_buffer_count(buffer_count),
-			m_permutations(m_sequences->size(), m_buffer_count, 0),
-			m_divergences(m_sequences->size(), m_buffer_count, 0),
-			m_divergence_value_counts(extra_fields & context_field::DIVERGENCE_VALUE_COUNTS ? m_buffer_count : 0),
-			m_character_counts(m_alphabet->sigma(), 0),
-			m_previous_positions(1 + m_alphabet->sigma(), 0)
-		{
-			auto const seq_length(sequence_length());
-			m_fields_in_use = static_cast <context_field const>(m_fields_in_use | extra_fields);
-			for (auto &list : m_divergence_value_counts)
-				list.resize(1 + seq_length);
-		}
-		
-		std::size_t sequence_idx() const { return m_sequence_idx; }
-		std::size_t const size() const { return m_sequences->size(); }
-		std::size_t const sequence_length() const { return m_sequences->front().size(); }
-		std::size_t const buffer_count() const { return m_buffer_count; }
-		divergence_count_list const &last_divergence_value_counts() const;
-		sample_vector &samples() { return m_samples; }
-		sample_vector const &samples() const { return m_samples; }
-		
-		typename string_index_matrix::const_slice_type const &permutation(std::size_t const idx) const { return m_permutations.column(idx); }
-		character_index_vector const &divergence(std::size_t const idx) const { return m_divergences.column(idx); }
-		divergence_count_list const &divergence_value_counts(std::size_t const idx) const { return m_divergence_value_counts[idx]; }
-		
-		std::size_t src_buffer_idx() const { return (m_buffer_count + m_sequence_idx - 1) % m_buffer_count; }
-		std::size_t dst_buffer_idx() const { return m_sequence_idx % m_buffer_count; }
-		
-		void set_sample_rate(std::uint64_t const rate);
-		void set_fields_in_use(context_field fields) { m_fields_in_use = fields; }
-		
-		void prepare();
-		
-		void release_lock() { dispatch_semaphore_signal(*m_process_sema); }
-		
-		template <
-			bool t_use_semaphore,
-			context_field t_extra_fields,
-			typename t_callback_fn,
-			typename t_continue_fn,
-			typename t_dispatch_copy_sample_fn
-		>
-		void process(
-			t_callback_fn &&callback_fn,
-			t_continue_fn &&continue_fn,
-			t_dispatch_copy_sample_fn &&dispatch_copy_sample_fn
-		) { process <t_use_semaphore, t_extra_fields>(SIZE_MAX, callback_fn, continue_fn, dispatch_copy_sample_fn); }
-			
-		template <
-			bool t_use_semaphore,
-			context_field t_extra_fields,
-			typename t_callback_fn,
-			typename t_continue_fn,
-			typename t_dispatch_copy_sample_fn
-		>
-		void process(
-			std::size_t const caller_limit,
-			t_callback_fn &&callback_fn,
-			t_continue_fn &&continue_fn,
-			t_dispatch_copy_sample_fn &&dispatch_copy_sample_fn
-		);
-		
-	protected:
-		template <context_field t_extra_fields>
-		void copy_sample(std::size_t src_idx, std::size_t dst_idx, sample_context_type &dst) const;
-	};
-	
-	
 	LIBBIO_PBWT_CONTEXT_TEMPLATE_DECL
 	void LIBBIO_PBWT_CONTEXT_CLASS_DECL::prepare()
 	{
@@ -520,7 +265,6 @@ namespace libbio { namespace pbwt {
 		m_sequences = nullptr;
 		m_alphabet = nullptr;
 		
-		// FIXME: this is slightly problematic since the vectors need not be std::vectors.
 		if (! (context_field::INPUT_PERMUTATION & m_fields_in_use))
 			clear_and_resize_vector(m_input_permutation);
 		
@@ -680,7 +424,6 @@ namespace libbio { namespace pbwt {
 	
 	LIBBIO_PBWT_CONTEXT_TEMPLATE_DECL
 	template <
-		bool t_callback_type,	// For compatibility with the buffering variant.
 		context_field t_extra_fields,
 		typename t_callback_fn
 	>
@@ -703,7 +446,7 @@ namespace libbio { namespace pbwt {
 			if (t_extra_fields & context_field::DIVERGENCE_VALUE_COUNTS)
 				update_divergence_value_counts();
 			
-			detail::callback_function_helper <t_callback_type>::call(*this, callback_fn);
+			callback_fn();
 			
 			// Check if a sample needs to be copied.
 			if (0 == m_sequence_idx % m_sample_rate)
@@ -714,195 +457,10 @@ namespace libbio { namespace pbwt {
 				++sample.m_sequence_idx;
 				sample.swap_input_and_output();
 			}
-
+			
 			swap_input_and_output();
 			++m_sequence_idx;
 		}
-	}
-	
-	
-	LIBBIO_PBWT_BUFFERING_PBWT_CONTEXT_TEMPLATE_DECL
-	auto LIBBIO_PBWT_BUFFERING_PBWT_CONTEXT_CLASS_DECL::last_divergence_value_counts() const -> divergence_count_list const &
-	{
-		auto const idx((m_sequence_idx - 1) % m_buffer_count);
-		assert(idx < m_divergence_value_counts.size());
-		return m_divergence_value_counts[idx];
-	}
-	
-	
-	LIBBIO_PBWT_BUFFERING_PBWT_CONTEXT_TEMPLATE_DECL
-	void LIBBIO_PBWT_BUFFERING_PBWT_CONTEXT_CLASS_DECL::prepare()
-	{
-		auto const last(m_buffer_count - 1);
-		auto input_permutation(m_permutations.column(last));
-		auto input_divergence(m_divergences.column(last));
-		auto &divergence_value_counts(m_divergence_value_counts[last]);
-		
-		// Fill the initial permutation and divergence.
-		std::iota(input_permutation.begin(), input_permutation.end(), 0);
-		std::fill(input_divergence.begin(), input_divergence.end(), 0);
-		
-		divergence_value_counts.link(m_sequences->size(), 0);
-		divergence_value_counts.set_last_element(0);
-	}
-	
-	
-	LIBBIO_PBWT_BUFFERING_PBWT_CONTEXT_TEMPLATE_DECL
-	template <
-		bool t_use_semaphore,
-		context_field t_extra_fields,
-		typename t_callback_fn,
-		typename t_continue_fn,
-		typename t_dispatch_copy_sample_fn
-	>
-	void LIBBIO_PBWT_BUFFERING_PBWT_CONTEXT_CLASS_DECL::process(
-		std::size_t const caller_limit,
-		t_callback_fn &&callback_fn,
-		t_continue_fn &&continue_fn,
-		t_dispatch_copy_sample_fn &&dispatch_copy_sample_fn
-	)
-	{
-		// The algorithm does not work if the number of the source buffer for copying a sample
-		// cannot be stored for the duration of filling all the buffers in-between.
-		assert(m_buffer_count <= m_sample_rate);
-		
-		auto const seq_length(sequence_length());
-		m_samples.reserve(1 + seq_length / m_sample_rate);
-		
-		// Create a semaphore and a counter for copying the samples.
-		dispatch_ptr <dispatch_semaphore_t> copy_sample_sema(dispatch_semaphore_create(0));
-		std::size_t copy_sample_src_idx(SIZE_MAX);
-		
-		// First src is the rightmost vector.
-		std::size_t const limit(std::min({caller_limit, seq_length}));
-		auto src_idx(src_buffer_idx());
-		auto dst_idx(dst_buffer_idx());
-		
-		while (m_sequence_idx < limit)
-		{
-			assert(src_idx < m_buffer_count);
-			assert(dst_idx < m_buffer_count);
-			
-			// Wait until the consumer thread has processed the buffered arrays.
-			if (t_use_semaphore)
-				dispatch_semaphore_wait(*m_process_sema, DISPATCH_TIME_FOREVER);
-			
-			// Wait until the copying task is complete.
-			if (copy_sample_src_idx == dst_idx)
-			{
-				dispatch_semaphore_wait(*copy_sample_sema, DISPATCH_TIME_FOREVER);
-				copy_sample_src_idx = SIZE_MAX;
-			}
-			
-			auto input_permutation(m_permutations.column(src_idx));
-			auto output_permutation(m_permutations.column(dst_idx));
-			auto input_divergence(m_divergences.column(src_idx));
-			auto output_divergence(m_divergences.column(dst_idx));
-			auto const &input_divergence_value_counts(m_divergence_value_counts[src_idx]);
-			auto &output_divergence_value_counts(m_divergence_value_counts[dst_idx]);
-			
-			// Prepare input_divergence for Range Maximum Queries.
-			character_index_vector_rmq input_divergence_rmq(&input_divergence);
-			
-			pbwt::build_prefix_and_divergence_arrays(
-				*m_sequences,
-				m_sequence_idx,
-				*m_alphabet,
-				input_permutation,
-				input_divergence,
-				input_divergence_rmq,
-				output_permutation,
-				output_divergence,
-				m_character_counts,
-				m_previous_positions
-			);
-			
-			// Copy the previous values and update.
-			if (t_extra_fields & context_field::DIVERGENCE_VALUE_COUNTS)
-			{
-				output_divergence_value_counts = input_divergence_value_counts;
-				pbwt::update_divergence_value_counts(
-					input_divergence,
-					output_divergence,
-					output_divergence_value_counts
-				);
-			}
-			
-			// Callback should take different arguments if m_process_sema is to be used.
-			detail::callback_function_helper_bc <t_use_semaphore>::call(*this, callback_fn, dst_idx);
-			
-			// Check if a sample needs to be copied.
-			if (0 == m_sequence_idx % m_sample_rate)
-			{
-				// Remember the position.
-				copy_sample_src_idx = src_idx;
-				
-				// Variables for the block.
-				// Having a reference to an element of m_samples should be safe
-				// b.c. the next copying task will wait in the beginning of the
-				// while loop for dispatch_semaphore_signal below.
-				auto &sample(m_samples.emplace_back());
-				auto sample_ptr(&sample);
-				auto sema(*copy_sample_sema); // Get the stored pointer.
-				
-				dispatch_copy_sample_fn(^{
-					// sample will be a reference.
-					copy_sample <t_extra_fields>(src_idx, dst_idx, *sample_ptr);
-					++sample_ptr->m_sequence_idx;
-					sample_ptr->swap_input_and_output();
-					
-					dispatch_semaphore_signal(sema);
-				});
-			}
-			
-			src_idx = dst_idx;
-			++dst_idx;
-			dst_idx %= m_buffer_count;
-			++m_sequence_idx;
-		}
-		
-		// Wait for copying if needed.
-		if (SIZE_MAX != copy_sample_src_idx)
-			dispatch_semaphore_wait(*copy_sample_sema, DISPATCH_TIME_FOREVER);
-		
-		continue_fn();
-	}
-	
-	
-	LIBBIO_PBWT_BUFFERING_PBWT_CONTEXT_TEMPLATE_DECL
-	void LIBBIO_PBWT_BUFFERING_PBWT_CONTEXT_CLASS_DECL::set_sample_rate(std::uint64_t const sample_rate)
-	{
-		//libbio_always_assert(0 == (sample_rate & (sample_rate - 1)), "Expected sample rate to be a power of two.");
-		m_sample_rate = max_ct(m_buffer_count, sample_rate);
-	}
-	
-	
-	LIBBIO_PBWT_BUFFERING_PBWT_CONTEXT_TEMPLATE_DECL
-	template <context_field t_extra_fields>
-	void LIBBIO_PBWT_BUFFERING_PBWT_CONTEXT_CLASS_DECL::copy_sample(std::size_t const src_idx, std::size_t const dst_idx, sample_context_type &dst) const
-	{
-		dst.m_sequences = m_sequences;
-		dst.m_alphabet = m_alphabet;
-		dst.m_sequence_idx = m_sequence_idx;
-		
-		auto const &input_permutation(m_permutations.column(src_idx));
-		auto const &output_permutation(m_permutations.column(dst_idx));
-		auto const &input_divergence(m_divergences.column(src_idx));
-		auto const &output_divergence(m_divergences.column(dst_idx));
-		auto const &divergence_value_counts(m_divergence_value_counts[dst_idx]);
-		
-		resize_and_copy(input_permutation, dst.m_input_permutation);
-		resize_and_copy(output_permutation, dst.m_output_permutation);
-		
-		// FIXME: inverse input permutation.
-		
-		resize_and_copy(input_divergence, dst.m_input_divergence);
-		resize_and_copy(output_divergence, dst.m_output_divergence);
-		resize_and_copy(m_character_counts, dst.m_character_counts);
-		resize_and_copy(m_previous_positions, dst.m_previous_positions);
-		
-		if (t_extra_fields & context_field::DIVERGENCE_VALUE_COUNTS)
-			dst.m_divergence_value_counts = divergence_value_counts;
 	}
 }}
 
