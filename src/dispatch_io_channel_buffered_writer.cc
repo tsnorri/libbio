8fc1c8345540d143e7f6df71321672cd30d660eb
tsnorri <tsnorri@iki.fi>
2021-03-10 16:09:56 +0200

    Clarify comments

--- src/dispatch_io_channel_buffered_writer.cc
+++ src/dispatch_io_channel_buffered_writer.cc
@@ -15,6 +15,7 @@ namespace libbio {
 		auto const byte_count(this->m_position);
 		if (byte_count)
 		{
+			// FIXME: make sure that nothing below throws, since the destructor of the dispatch data below is responsible for unlocking the semaphore.
 			m_writing_lock.lock();
 		
 			// Swap the buffers.
@@ -24,17 +25,17 @@ namespace libbio {
 			// Mark the original buffer empty.
 			this->m_position = 0;
 			
-			// FIXME: make sure that nothing below throws, since the destructor of the dispatch data below is responsible for unlocking the semaphore.
-			auto const *buffer(m_writing_buffer.data());
-		
 			// Create dispatch data and schedule a writing operation.
 			auto concurrent_queue(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0));
+			
 			dispatch_ptr dispatch_data(dispatch_data_create(buffer, byte_count, concurrent_queue, ^{ this->m_writing_lock.unlock(); }));
+			auto const *buffer(m_writing_buffer.data());
+			
 			dispatch_io_write(*m_io_channel, this->m_output_position, *dispatch_data, concurrent_queue, ^(bool done, dispatch_data_t data, int error){
 				if (error)
 				{
 					dispatch_async(*this->m_reporting_queue, ^{
-						throw std::runtime_error(strerror(error));
+						throw std::runtime_error(std::strerror(error));
 					});
 				}
 			});
