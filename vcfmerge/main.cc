2f79dd73e69f964f98c597d1ef8be556b3eafa4b
Tuukka Norri <tsnorri@iki.fi>
2021-03-18 12:38:43 +0200

    Implement merging samples
    – Earlier it was possible to only generate samples with predetermined GT values.

--- vcfmerge/main.cc
+++ vcfmerge/main.cc
@@ -24,6 +24,9 @@ namespace vcf	= libbio::vcf;
 
 namespace {
 	
+	typedef std::vector <merge::vcf_input>	vcf_input_vector;
+	
+	
 	struct sourced_variant
 	{
 		vcf::variant	variant;
@@ -38,6 +41,7 @@ namespace {
 		}
 	};
 	
+	typedef std::multiset <sourced_variant>	variant_set;
 	
 	bool operator<(sourced_variant const &lhs, sourced_variant const &rhs)
 	{
@@ -45,18 +49,7 @@ namespace {
 	}
 	
 	
-	// Generate a sample name for each of the given inputs.
-	void make_sample_names(std::vector <merge::vcf_input> const &inputs, std::vector <std::string> &out_sample_names)
-	{
-		for (auto const &input : inputs)
-		{
-			fs::path path(input.source_path);
-			out_sample_names.push_back(path.stem());
-		}
-	}
-
-
-	void read_inputs(std::vector <merge::vcf_input> &inputs, char **names, std::size_t const count)
+	void read_inputs(vcf_input_vector &inputs, char **names, std::size_t const count)
 	{
 		inputs.resize(count);
 		for (std::size_t i(0); i < count; ++i)
@@ -65,7 +58,7 @@ namespace {
 			{
 				inputs[i].open_file(names[i]);
 			}
-			catch (std::runtime_error &exc)
+			catch (std::runtime_error const &exc)
 			{
 				std::cerr << "Unable to open file " << inputs[i].source_path << ": " << exc.what() << '\n';
 				std::exit(EXIT_FAILURE);
@@ -74,7 +67,8 @@ namespace {
 	}
 	
 	
-	void read_input_list(std::vector <merge::vcf_input> &inputs, char const *list_path)
+	// Read inputs from a list file.
+	void read_input_list(vcf_input_vector &inputs, char const *list_path)
 	{
 		std::vector <std::string> names;
 		std::ifstream list_file(list_path);
@@ -89,13 +83,221 @@ namespace {
 			{
 				input.open_file(std::move(name));
 			}
-			catch (std::runtime_error &exc)
+			catch (std::runtime_error const &exc)
 			{
 				std::cerr << "Unable to open file " << name << ": " << exc.what() << '\n';
 				std::exit(EXIT_FAILURE);
 			}
 		}
 	}
+	
+	
+	struct merge_strategy
+	{
+		typedef std::vector <std::string>	sample_name_vector;
+		
+		virtual ~merge_strategy() {}
+		virtual void make_sample_names(vcf_input_vector const &inputs, sample_name_vector &sample_names) const = 0;
+		virtual void prepare_variants(variant_set &variants, vcf_input_vector &inputs) = 0;
+		virtual void output_variant(std::size_t const input_idx, vcf::variant const &variant) = 0;
+		
+		void output_column_header(vcf_input_vector const &inputs) const
+		{
+			// Determine the sample names.
+			sample_name_vector sample_names;
+			make_sample_names(inputs, sample_names);
+			
+			// Output the column headers.
+			std::cout << "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT";
+			for (auto const &sample_name : sample_names)
+				std::cout << '\t' << sample_name;
+			std::cout << '\n';
+		}
+		
+		void output_variants(variant_set &variants, vcf_input_vector &inputs)
+		{
+			while (!variants.empty())
+			{
+				// Get the next variant.
+				auto node(variants.extract(variants.begin()));
+				auto &sourced_var(node.value());
+				auto const idx(sourced_var.input_index);
+				libbio_assert_lt(idx, inputs.size());
+				auto &input(inputs[idx]);
+				
+				output_variant(idx, sourced_var.variant);
+				
+				if (input.record_generator.get_next_variant(sourced_var.variant))
+					variants.insert(sourced_var);
+			}
+		}
+		
+		virtual void process_variants(vcf_input_vector &inputs)
+		{
+			// Output the records in order.
+			// Use a multiset in order to reuse the variants’ buffers and prepare it with the first variant from each input.
+			variant_set variants;
+			output_column_header(inputs);
+			prepare_variants(variants, inputs);
+			output_variants(variants, inputs);
+		}
+	};
+	
+	
+	class merge_gs_strategy final : public merge_strategy
+	{
+	protected:
+		merge::variant_printer_gs <vcf::variant>	m_printer;
+		
+	public:
+		merge_gs_strategy(std::size_t const input_count, std::size_t const sample_ploidy, bool const samples_are_phased):
+			m_printer(input_count, sample_ploidy, samples_are_phased)
+		{
+		}
+		
+		// Generate a sample name for each of the given inputs.
+		void make_sample_names(vcf_input_vector const &inputs, sample_name_vector &out_sample_names) const override
+		{
+			for (auto const &input : inputs)
+			{
+				fs::path path(input.source_path);
+				out_sample_names.push_back(path.stem());
+			}
+		}
+		
+		void prepare_variants(variant_set &variants, vcf_input_vector &inputs) override
+		{
+			for (auto const &[idx, input] : rsv::enumerate(inputs))
+			{
+				input.reader.set_parsed_fields(vcf::field::INFO);
+
+				auto var(input.reader.make_empty_variant());
+				if (input.record_generator.get_next_variant(var))
+					variants.emplace(std::move(var), idx);
+			}
+		}
+		
+		void output_variant(std::size_t const input_idx, vcf::variant const &variant) override
+		{
+			m_printer.set_active_sample_index(input_idx);
+			m_printer.output_variant(std::cout, variant);
+		}
+	};
+	
+	
+	class merge_ms_strategy final : public merge_strategy
+	{
+	protected:
+		merge::variant_printer_ms <vcf::variant>	m_printer;
+		std::vector <std::size_t>					m_sample_count_csum;
+		bool										m_should_merge_sample_names{};
+		
+	public:
+		merge_ms_strategy(
+			std::size_t const input_count,
+			std::size_t const sample_ploidy,
+			bool const samples_are_phased,
+			bool const should_merge_sample_names
+		):
+			m_printer(0, sample_ploidy, samples_are_phased), // Total samples not yet known.
+			m_sample_count_csum(1 + input_count, 0),
+			m_should_merge_sample_names(should_merge_sample_names)
+		{
+		}
+		
+		// Generate a sample name for each of the samples in the given inputs.
+		void make_sample_names(vcf_input_vector const &inputs, sample_name_vector &out_sample_names) const override
+		{
+			if (m_should_merge_sample_names)
+			{
+				// Count the total samples.
+				std::size_t total_samples(0);
+				for (auto const &input : inputs)
+					total_samples += input.reader.sample_count();
+				
+				out_sample_names.resize(total_samples);
+				
+				// Read the sample names and verify that they are unique.
+				std::set <std::string> all_sample_names;
+				std::size_t out_start_idx(0);
+				for (auto const &input : inputs)
+				{
+					auto const &reader(input.reader);
+					auto const &sample_names(reader.sample_names());
+					
+					for (auto const &[sample_name, sample_idx] : sample_names)
+					{
+						// Check that the name is distinct.
+						auto const res(all_sample_names.emplace(sample_name));
+						if (!res.second)
+						{
+							std::cerr << "ERROR: Duplicate sample name “" << sample_name << "”\n";
+							std::exit(EXIT_FAILURE);
+						}
+						
+						// Write to the output.
+						libbio_assert(sample_idx);
+						auto const out_idx(out_start_idx + sample_idx - 1);
+						libbio_assert_lt(out_idx, out_sample_names.size());
+						out_sample_names[out_idx] = sample_name;
+					}
+					
+					out_start_idx += sample_names.size();
+				}
+			}
+			else
+			{
+				for (auto const &input : inputs)
+				{
+					fs::path path(input.source_path);
+				
+					auto const count(input.reader.sample_count());
+					for (std::size_t i(0); i < count; ++i)
+					{
+						auto &name(out_sample_names.emplace_back(path.stem()));
+						name += '.';
+						name += (1 + i);
+					}
+				}
+			}
+		}
+		
+		void prepare_variants(variant_set &variants, vcf_input_vector &inputs) override
+		{
+			// Prepare a cumulative sum of the sample counts.
+			std::size_t sample_count_csum(0);
+			for (auto const &[idx, input] : rsv::enumerate(inputs))
+			{
+				auto const sample_count(input.reader.sample_count());
+				if (sample_count)
+				{
+					input.reader.set_parsed_fields(vcf::field::ALL);
+					
+					auto var(input.reader.make_empty_variant());
+					if (input.record_generator.get_next_variant(var))
+						variants.emplace(std::move(var), sample_count_csum);
+					
+					sample_count_csum += sample_count;
+				}
+				
+				auto const csidx(1 + idx);
+				libbio_assert_lt(csidx, m_sample_count_csum.size());
+				m_sample_count_csum[csidx] = sample_count_csum;
+			}
+			
+			// Update the total sample count.
+			m_printer.set_total_samples(sample_count_csum);
+		}
+		
+		void output_variant(std::size_t const input_idx, vcf::variant const &variant) override
+		{
+			libbio_assert_lt(1 + input_idx, m_sample_count_csum.size());
+			auto const lb(m_sample_count_csum[input_idx]);
+			auto const rb(m_sample_count_csum[1 + input_idx]);
+			m_printer.set_active_sample_range(lb, rb);
+			m_printer.output_variant(std::cout, variant);
+		}
+	};
 }
 
 
@@ -105,6 +307,7 @@ int main(int argc, char **argv)
 	if (0 != cmdline_parser(argc, argv, &args_info))
 		std::exit(EXIT_FAILURE);
 	
+	// FIXME: Currently we require that the user sets the sample ploidy even when merging samples. To determine the ploidy from the inputs, we would have to store the ploidy for each sample by reading the first variant record of each input.
 	if (args_info.sample_ploidy_arg < 1)
 	{
 		std::cerr << "Sample ploidy must be at least one.\n";
@@ -115,7 +318,7 @@ int main(int argc, char **argv)
 	std::ios_base::sync_with_stdio(false);	// Don't use C style IO after calling cmdline_parser.
 	
 	// Open the VCF files.
-	std::vector <merge::vcf_input> inputs;
+	vcf_input_vector inputs;
 	if (args_info.input_list_given)
 		read_input_list(inputs, args_info.input_list_arg);
 	else
@@ -170,56 +373,29 @@ int main(int argc, char **argv)
 			assembly.output_vcf(std::cout);
 	}
 	
+	// Output the format header for GT if it did not exist.
+	{
+		auto const &format_meta(format_checker.metadata_by_key());
+		if (format_meta.end() == format_meta.find("GT"))
+			std::cout << "##FORMAT=<ID=GT,Number=1,Type=String,Description=\"Genotype\">\n";
+	}
+	
 	// Merge the records and output.
+	// FIXME: Rewrite the ID column contents?
 	if (args_info.generate_samples_given)
 	{
-		std::vector <std::string> sample_names;
-		make_sample_names(inputs, sample_names);
+		merge_gs_strategy strategy(inputs.size(), sample_ploidy, args_info.samples_are_phased_flag);
+		strategy.process_variants(inputs);
+	}
+	else if (args_info.merge_samples_given)
+	{
+		std::cerr << "NOTE: Merging mixed ploidies (e.g. samples with two X chromosomes and X and Y chromosomes) has not been implemented.\n";
 		
-		{
-			auto const &format_meta(format_checker.metadata_by_key());
-			if (format_meta.end() == format_meta.find("GT"))
-				std::cout << "##FORMAT=<ID=GT,Number=1,Type=String,Description=\"Genotype\">\n";
-		}
-		
-		// Output the column headers.
-		std::cout << "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT";
-		for (auto const &sample_name : sample_names)
-			std::cout << '\t' << sample_name;
-		std::cout << '\n';
-		
-		// Output the records in order.
-		// Use a multiset in order to reuse the variants’ buffers.
-		std::multiset <sourced_variant> variants;
-		for (auto const &[idx, input] : rsv::enumerate(inputs))
-		{
-			input.reader.set_parsed_fields(vcf::field::INFO);
-			
-			auto var(input.reader.make_empty_variant());
-			if (input.record_generator.get_next_variant(var))
-				variants.emplace(std::move(var), idx);
-		}
-		
-		merge::variant_printer <vcf::variant> printer(inputs.size(), sample_ploidy, args_info.samples_are_phased_flag);
-		while (!variants.empty())
-		{
-			// Get the next variant.
-			auto node(variants.extract(variants.begin()));
-			auto &sourced_var(node.value());
-			auto const idx(sourced_var.input_index);
-			libbio_assert_lt(idx, inputs.size());
-			auto &input(inputs[idx]);
-			
-			printer.set_active_sample_index(idx);
-			printer.output_variant(std::cout, sourced_var.variant);
-			
-			if (input.record_generator.get_next_variant(sourced_var.variant))
-				variants.insert(sourced_var);
-		}
+		merge_ms_strategy strategy(inputs.size(), sample_ploidy, args_info.samples_are_phased_flag, !args_info.rename_samples_given);
+		strategy.process_variants(inputs);
 	}
 	else
 	{
-		// TODO: Merge the sample names.
 		std::cerr << "Not implemented.\n";
 		return EXIT_FAILURE;
 	}
--- vcfmerge/variant_printer.hh
+++ vcfmerge/variant_printer.hh
@@ -12,27 +12,27 @@
 
 namespace libbio::vcfmerge {
 	
+	// Base class for variant printers.
 	template <typename t_variant>
-	class variant_printer final : public vcf::variant_printer_base <t_variant>
+	class variant_printer_base : public vcf::variant_printer_base <t_variant>
 	{
 	public:
 		typedef typename vcf::variant_printer_base <t_variant>::variant_type variant_type;
 		
 	protected:
 		std::size_t	m_total_samples{};
-		std::size_t	m_active_sample_index{};
 		std::size_t	m_sample_ploidy{};
 		bool		m_samples_are_phased{};
 		
 	public:
-		variant_printer(std::size_t const total_samples, std::size_t const sample_ploidy, bool const samples_are_phased):
+		variant_printer_base(std::size_t const total_samples, std::size_t const sample_ploidy, bool const samples_are_phased):
 			m_total_samples(total_samples),
 			m_sample_ploidy(sample_ploidy),
 			m_samples_are_phased(samples_are_phased)
 		{
 		}
 		
-		void set_active_sample_index(std::size_t const idx) { m_active_sample_index = idx; }
+		void set_total_samples(std::size_t const total_samples) { m_total_samples = total_samples; }
 		
 		void output_format(std::ostream &os, variant_type const &var) const override
 		{
@@ -54,17 +54,86 @@ namespace libbio::vcfmerge {
 				os << separator << is_active;
 		}
 		
+	protected:
+		void output_sample_range(std::ostream &os, std::size_t lb, std::size_t const rb, bool &is_first) const
+		{
+			char const separator(m_samples_are_phased ? '|' : '/');
+			while (lb < rb)
+			{
+				output_sample(os, false, separator, is_first);
+				++lb;
+			}
+		}
+	};
+	
+	
+	template <typename t_variant>
+	class variant_printer_gs final : public variant_printer_base <t_variant>
+	{
+	public:
+		typedef typename variant_printer_base <t_variant>::variant_type	variant_type;
+		
+	protected:
+		std::size_t	m_active_sample_index{};
+		
+	public:
+		using variant_printer_base <t_variant>::variant_printer_base;
+		
+		void set_active_sample_index(std::size_t const idx) { m_active_sample_index = idx; }
+		
 		void output_samples(std::ostream &os, variant_type const &var) const override
 		{
 			bool is_first(true);
-			char const separator(m_samples_are_phased ? '|' : '/');
-			for (std::size_t i(0); i < m_active_sample_index; ++i)
-				output_sample(os, false, separator, is_first);
+			char const separator(this->m_samples_are_phased ? '|' : '/');
 			
-			output_sample(os, true, separator, is_first);
+			this->output_sample_range(os, 0, m_active_sample_index, is_first);
+			this->output_sample(os, true, separator, is_first);
+			this->output_sample_range(os, 1 + m_active_sample_index, this->m_total_samples, is_first);
+		}
+	};
+	
+	
+	template <typename t_variant>
+	class variant_printer_ms final : public variant_printer_base <t_variant>
+	{
+	public:
+		typedef typename variant_printer_base <t_variant>::variant_type	variant_type;
+		
+	protected:
+		std::size_t	m_active_sample_lb{};
+		std::size_t	m_active_sample_rb{};
+		
+	public:
+		using variant_printer_base <t_variant>::variant_printer_base;
+		
+		void set_active_sample_range(std::size_t const lb, std::size_t const rb) { m_active_sample_lb = lb; m_active_sample_rb = rb; }
+		
+		void output_samples(std::ostream &os, variant_type const &var) const override
+		{
+			auto const &samples(var.samples());
+			libbio_assert_eq(m_active_sample_rb - m_active_sample_lb, samples.size());
 			
-			for (std::size_t i(1 + m_active_sample_index); i < m_total_samples; ++i)
-				output_sample(os, false, separator, is_first);
+			// Get the GT field.
+			auto const &fields(var.get_format().fields_by_identifier());
+			auto const it(fields.find("GT"));
+			libbio_always_assert_neq_msg(it, fields.end(), "Expected variant to have a GT field");
+			auto const &gt_field(*it->second);
+			
+			// Output the samples.
+			bool is_first(true);
+			this->output_sample_range(os, 0, m_active_sample_lb, is_first);
+			
+			for (auto const &sample : samples)
+			{
+				if (is_first)
+					is_first = false;
+				else
+					os << '\t';
+				
+				gt_field.output_vcf_value(os, sample);
+			}
+			
+			this->output_sample_range(os, m_active_sample_rb, this->m_total_samples, is_first);
 		}
 	};
 }
