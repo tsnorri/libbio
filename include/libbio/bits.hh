798b469eff76568858f941569b4915e614cead68
Tuukka Norri <tsnorri@iki.fi>
2022-06-07 19:21:49 +0300

    Distinguish zero value and zero traiilng zeros in trailing_zeros()

--- include/libbio/bits.hh
+++ include/libbio/bits.hh
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 Tuukka Norri
+ * Copyright (c) 2018-2022 Tuukka Norri
  * This code is licensed under MIT license (see LICENSE for details).
  */
 
@@ -12,26 +12,29 @@
 
 namespace libbio { namespace bits {
 	
-	
+	// Starting from the least significant bit position.
+	// WARNING: These have been changed s.t. if the parameter has at least one bit set,
+	// the returned value is non-zero.
 	inline std::uint8_t trailing_zeros(unsigned int const i)
 	{
 		if (0 == i) return 0;
-		return __builtin_ctz(i);
+		return 1 + __builtin_ctz(i);
 	}
 	
 	inline std::uint8_t trailing_zeros(unsigned long const l)
 	{
 		if (0 == l) return 0;
-		return __builtin_ctzl(l);
+		return 1 + __builtin_ctzl(l);
 	}
 	
 	inline std::uint8_t trailing_zeros(unsigned long long const ll)
 	{
 		if (0 == ll) return 0;
-		return __builtin_ctzll(ll);
+		return 1 + __builtin_ctzll(ll);
 	}
 	
 	
+	// Starting from the most significant bit position.
 	inline std::uint8_t leading_zeros(unsigned int const i)
 	{
 		if (0 == i) return (CHAR_BIT * sizeof(unsigned int));
