3c03970bb32efb8c2702b9784b79ac5bca8bfa14
Tuukka Norri <tsnorri@iki.fi>
2022-08-16 15:18:57 +0300

    Add support for stdout, stderr

--- include/libbio/subprocess.hh
+++ include/libbio/subprocess.hh
@@ -6,15 +6,109 @@
 #ifndef LIBBIO_SUBPROCESS_HH
 #define LIBBIO_SUBPROCESS_HH
 
+#include <libbio/assert.hh>
+#include <libbio/bits.hh>
 #include <libbio/file_handle.hh>
+#include <libbio/utility/misc.hh>
 #include <limits>
+#include <range/v3/view/enumerate.hpp>
 #include <tuple>
+
+namespace libbio {
+	
+	enum class subprocess_handle_spec : std::uint8_t
+	{
+		NONE	= 0x0,
+		STDIN	= 0x1,
+		STDOUT	= 0x2,
+		STDERR	= 0x4
+	};
+	
+	constexpr inline subprocess_handle_spec operator|(subprocess_handle_spec const lhs, subprocess_handle_spec const rhs)
+	{
+		return static_cast <subprocess_handle_spec>(to_underlying(lhs) | to_underlying(rhs));
+	}
+	
+	constexpr inline subprocess_handle_spec operator&(subprocess_handle_spec const lhs, subprocess_handle_spec const rhs)
+	{
+		return static_cast <subprocess_handle_spec>(to_underlying(lhs) & to_underlying(rhs));
+	}
+	
+	template <subprocess_handle_spec t_spec, subprocess_handle_spec t_h>
+	constexpr inline bool has_handle() { return to_underlying(t_spec & t_h) ? true : false; }
+}
 
 
 namespace libbio { namespace detail {
 	
-	std::tuple <pid_t, int>
-	open_subprocess_for_writing(char * const args[]); // nullptr-terminated list of arguments.
+	std::tuple <pid_t, int, int, int>
+	open_subprocess(char * const args[], subprocess_handle_spec const handle_spec); // nullptr-terminated list of arguments.
+	
+	
+	consteval std::size_t handle_count(subprocess_handle_spec const t_spec)
+	{
+		return bits::count_bits_set(to_underlying(t_spec));
+	}
+	
+	
+	// Calculate the rank of curr in all.
+	constexpr std::size_t handle_index(subprocess_handle_spec const curr, subprocess_handle_spec const all)
+	{
+		typedef std::underlying_type_t <subprocess_handle_spec> underlying_t;
+		
+		// Require that there is a (non-zero) handle and it is in the given set.
+		if (!to_underlying(curr & all))
+			throw std::invalid_argument("Given handle specification is either zero or not in the set of all specifications");
+		
+		// Determine the 1-based index of the current value.
+		auto const bit_idx(bits::highest_bit_set(to_underlying(curr)));
+		
+		// Make a mask of the lower bits.
+		underlying_t mask(~underlying_t(0));
+		mask >>= sizeof(underlying_t) * CHAR_BIT - bit_idx;
+		mask >>= 0x1;
+		
+		// Count the lower bits.
+		underlying_t const lower_bits(to_underlying(curr) & mask);
+		auto const lower_count(bits::count_bits_set(lower_bits));
+		
+		return lower_count;
+	}
+	
+	
+	template <std::size_t t_n, std::size_t... t_i>
+	std::array <libbio::file_handle, t_n>
+	make_handle_array_(std::array <int, t_n> const &fds, std::index_sequence <t_i...>)
+	{
+		return {{libbio::file_handle(std::get <t_i>(fds))...}};
+	}
+	
+	
+	template <std::size_t t_n, typename t_idxs = std::make_index_sequence <t_n>>
+	std::array <libbio::file_handle, t_n>
+	make_handle_array(std::array <int, t_n> const &fds)
+	{
+		return make_handle_array_(fds, t_idxs{});
+	}
+	
+	
+	template <subprocess_handle_spec t_spec, std::size_t t_handle_count = handle_count(t_spec)>
+	std::array <libbio::file_handle, t_handle_count>
+	make_handle_array(std::tuple <pid_t, int, int, int> const &pid_fd_tuple)
+	{
+		typedef std::underlying_type_t <subprocess_handle_spec> underlying_t;
+		std::array <int, t_handle_count> fds;
+		
+		// Compile-time check everything.
+		if constexpr (to_underlying(subprocess_handle_spec::STDIN & t_spec))
+			std::get <handle_index(subprocess_handle_spec::STDIN,  t_spec)>(fds) = std::get <1>(pid_fd_tuple);
+		if constexpr (to_underlying(subprocess_handle_spec::STDOUT & t_spec))
+			std::get <handle_index(subprocess_handle_spec::STDOUT, t_spec)>(fds) = std::get <2>(pid_fd_tuple);
+		if constexpr (to_underlying(subprocess_handle_spec::STDERR & t_spec))
+			std::get <handle_index(subprocess_handle_spec::STDERR, t_spec)>(fds) = std::get <3>(pid_fd_tuple);
+		
+		return make_handle_array(fds);
+	}
 }}
 
 
@@ -61,33 +155,58 @@ namespace libbio {
 	};
 	
 	
+	std::vector <std::string> parse_command_arguments(char const *args);
+	
+	
+	template <subprocess_handle_spec t_handle_spec>
 	class subprocess
 	{
 	public:
 		typedef process_handle::close_return_t	close_return_t;
+		typedef std::array <
+			file_handle,
+			detail::handle_count(t_handle_spec)
+		>										handle_array;
 		
 	protected:
 		process_handle	m_process{};		// Data member destructors called in reverse order; important for closing the pipe first.
-		file_handle		m_write_handle{};
+		handle_array	m_handles;
 		
 	protected:
-		explicit subprocess(std::tuple <pid_t, int> const &pid_fd_tuple):
+		explicit subprocess(std::tuple <pid_t, int, int, int> const &pid_fd_tuple):
 			m_process(std::get <0>(pid_fd_tuple)),
-			m_write_handle(std::get <1>(pid_fd_tuple))
+			m_handles(detail::make_handle_array <t_handle_spec>(pid_fd_tuple))
 		{
 		}
-	
+		
+		template <subprocess_handle_spec t_spec>
+		constexpr static bool has_handle() { return libbio::has_handle <t_handle_spec, t_spec>(); }
+		
+		constexpr static std::size_t handle_index(subprocess_handle_spec const spec) { return detail::handle_index(spec, t_handle_spec); }
+		template <subprocess_handle_spec t_spec> constexpr file_handle       &handle()       { return std::get <handle_index(t_spec)>(m_handles); }
+		template <subprocess_handle_spec t_spec> constexpr file_handle const &handle() const { return std::get <handle_index(t_spec)>(m_handles); }
+		
 	public:
 		subprocess() = default;
 		
-		explicit subprocess(char * const args[]):
-			subprocess(detail::open_subprocess_for_writing(args))
+		// The second argument is useful for having a set of subprocess instances of the same size.
+		explicit subprocess(char * const args[], subprocess_handle_spec const spec = t_handle_spec):
+			subprocess(detail::open_subprocess(args, spec))
 		{
+			libbio_assert_eq(0x0, to_underlying(spec) & ~to_underlying(t_handle_spec));
 		}
 		
+		static subprocess subprocess_with_arguments(std::vector <std::string> &args, subprocess_handle_spec const spec = t_handle_spec);
+		static subprocess subprocess_with_arguments(char const * const args, subprocess_handle_spec const spec = t_handle_spec);
+		
 		inline close_return_t close();
-		file_handle &write_handle() { return m_write_handle; }
-		file_handle const &write_handle() const { return m_write_handle; }
+		
+		file_handle       &stdin_handle()        requires (has_handle <subprocess_handle_spec::STDIN>())  { return handle <subprocess_handle_spec::STDIN>(); }
+		file_handle const &stdin_handle()  const requires (has_handle <subprocess_handle_spec::STDIN>())  { return handle <subprocess_handle_spec::STDIN>(); }
+		file_handle       &stdout_handle()       requires (has_handle <subprocess_handle_spec::STDOUT>()) { return handle <subprocess_handle_spec::STDOUT>(); }
+		file_handle const &stdout_handle() const requires (has_handle <subprocess_handle_spec::STDOUT>()) { return handle <subprocess_handle_spec::STDOUT>(); }
+		file_handle       &stderr_handle()       requires (has_handle <subprocess_handle_spec::STDERR>()) { return handle <subprocess_handle_spec::STDERR>(); }
+		file_handle const &stderr_handle() const requires (has_handle <subprocess_handle_spec::STDERR>()) { return handle <subprocess_handle_spec::STDERR>(); }
 	};
 	
 	
@@ -103,10 +222,31 @@ namespace libbio {
 	}
 	
 	
-	auto subprocess::close() -> close_return_t
+	template <subprocess_handle_spec t_handle_spec>
+	auto subprocess <t_handle_spec>::close() -> close_return_t
 	{
-		m_write_handle.close();
+		std::apply([](auto... handle) { ((handle.close()), ...); }, m_handles);
 		return m_process.close();
+	}
+	
+	
+	template <subprocess_handle_spec t_handle_spec>
+	subprocess <t_handle_spec> subprocess <t_handle_spec>::subprocess_with_arguments(std::vector <std::string> &args, subprocess_handle_spec const spec)
+	{
+		auto const argc(args.size());
+		char *argv[argc + 1];
+		for (auto &&[i, arg] : ranges::views::enumerate(args))
+			argv[i] = arg.data();
+		argv[argc] = nullptr;
+		return subprocess <t_handle_spec>(argv, spec);
+	}
+	
+	
+	template <subprocess_handle_spec t_handle_spec>
+	subprocess <t_handle_spec> subprocess <t_handle_spec>::subprocess_with_arguments(char const * const args, subprocess_handle_spec const spec)
+	{
+		auto parsed_args(parse_command_arguments(args));
+		return subprocess_with_arguments(parsed_args);
 	}
 }
 
--- src/subprocess.cc
+++ src/subprocess.cc
@@ -13,26 +13,61 @@
 
 namespace libbio { namespace detail {
 	
-	std::tuple <pid_t, int>
-	open_subprocess_for_writing(char * const args[]) // nullptr-terminated list of arguments.
+	static const std::tuple <std::size_t, int, int>
+	s_values_for_handle_spec[]{
+		{0, STDIN_FILENO, O_RDONLY},
+		{1, STDOUT_FILENO, O_WRONLY},
+		{1, STDERR_FILENO, O_WRONLY}
+	};
+	
+	
+	inline std::tuple <std::size_t, int, int> values_for_handle_spec(subprocess_handle_spec const handle_spec)
+	{
+		auto const idx1(to_underlying(handle_spec));
+		libbio_assert_lt(0, idx1);
+		auto const idx(idx1 - 1);
+		libbio_assert_lt(idx, sizeof(s_values_for_handle_spec) / sizeof(s_values_for_handle_spec[0]));
+		return s_values_for_handle_spec[idx];
+	}
+	
+	
+	std::tuple <pid_t, int, int, int>
+	open_subprocess(char * const args[], subprocess_handle_spec const handle_spec) // nullptr-terminated list of arguments.
 	{
 		// Note that if this function is invoked simultaneously from different threads,
 		// the subprocesses can leak file descriptors b.c. the ones resulting from the
 		// call to pipe() may not have been closed or marked closed-on-exec.
 		
-		// Create a pipe for writing only for now.
-		int fd[2];
-		if (0 != ::pipe(fd))
-			throw std::runtime_error(::strerror(errno));
+		// Create the requested pipes.
+		int fds[3][2]{{-1, -1}, {-1, -1}, {-1, -1}};
+		
+		auto process_all_fds([&fds](auto &&cb){
+			for (std::size_t i(0); i < 3; ++i)
+				cb(static_cast <subprocess_handle_spec>(1 + i), fds[i]);
+		});
+		
+		auto process_open_fds([&process_all_fds, handle_spec](auto &&cb){
+			process_all_fds([&cb, handle_spec](auto const curr_spec, auto &fd_pair){
+				if (to_underlying(curr_spec & handle_spec))
+					cb(curr_spec, fd_pair);
+			});
+		});
+		
+		process_open_fds([](auto const, auto &fd_pair){
+			if (0 != ::pipe(fd_pair))
+				throw std::runtime_error(::strerror(errno));
+		});
 		
 		auto const pid(::fork());
 		switch (pid)
 		{
 			case -1:
 			{
-				auto const *err(::strerror(errno));
-				::close(fd[0]);
-				::close(fd[1]);
+				auto const * const err(::strerror(errno));
+				process_open_fds([](auto const, auto &fd_pair){
+					::close(fd_pair[0]);
+					::close(fd_pair[1]);
+				});
 				throw std::runtime_error(err);
 				break;
 			}
@@ -42,15 +77,21 @@ namespace libbio { namespace detail {
 				// Try to make the child continue when debugging.
 				::signal(SIGTRAP, SIG_IGN);
 				
-				if (-1 != ::close(STDOUT_FILENO))							::_exit(69);	// EX_UNAVAILABLE in sysexits.h.
-				if (-1 != ::close(STDERR_FILENO))							::_exit(69);
-				
-				if (-1 != ::dup2(fd[0], STDIN_FILENO))						::_exit(69);
-				if (-1 != ::openat(STDOUT_FILENO, "/dev/null", O_WRONLY))	::_exit(69);
-				if (-1 != ::openat(STDERR_FILENO, "/dev/null", O_WRONLY))	::_exit(69);
-				
-				if (-1 != ::close(fd[0]))									::_exit(69);
-				if (-1 != ::close(fd[1]))									::_exit(69);
+				process_all_fds([handle_spec](auto const curr_spec, auto &fd_pair){
+					auto const [sp_fd_idx, fno, open_flags] = detail::values_for_handle_spec(curr_spec);
+					
+					if (to_underlying(curr_spec & handle_spec))
+					{
+						if (-1 != ::dup2(fd_pair[sp_fd_idx], fno))			::_exit(69);	// EX_UNAVAILABLE in sysexits.h.
+						if (-1 != ::close(fd_pair[0]))						::_exit(69);
+						if (-1 != ::close(fd_pair[1]))						::_exit(69);
+					}
+					else
+					{
+						if (-1 != ::close(fno))								::_exit(69);
+						if (-1 != ::openat(fno, "/dev/null", open_flags))	::_exit(69);
+					}
+				});
 				
 				::execvp(*args, args);
 				
@@ -82,13 +123,32 @@ namespace libbio { namespace detail {
 			
 			default: // Parent.
 			{
-				::close(fd[0]);
+				bool should_throw{};
+				char const *err{};
+				process_open_fds([&should_throw, &err](auto const curr_spec, auto &fd_pair){
+					auto const [sp_fd_idx, fno, open_flags] = detail::values_for_handle_spec(curr_spec);
+					auto const parent_fd_idx(1 - sp_fd_idx);
+					
+					::close(fd_pair[sp_fd_idx]);
+					if (-1 == ::fcntl(fd_pair[parent_fd_idx], F_SETFD, FD_CLOEXEC))
+					{
+						if (!should_throw)
+						{
+							err = ::strerror(errno);
+							should_throw = true;
+						}
+					}
+				});
 				
-				// Enable close-on-exec for the writing end.
-				if (-1 == ::fcntl(fd[1], F_SETFD, FD_CLOEXEC))
+				if (should_throw)
 				{
-					auto const *err(::strerror(errno));
-					::close(fd[1]);
+					// Clean up if there was an error.
+					process_open_fds([](auto const curr_spec, auto &fd_pair){
+						auto const [sp_fd_idx, fno, open_flags] = detail::values_for_handle_spec(curr_spec);
+						auto const parent_fd_idx(1 - sp_fd_idx);
+						::close(fd_pair[parent_fd_idx]);
+					});
+					
 					throw std::runtime_error(err);
 				}
 				
@@ -96,7 +156,7 @@ namespace libbio { namespace detail {
 			}
 		}
 		
-		return {pid, fd[1]};
+		return {pid, fds[0][1], fds[1][0], fds[2][0]}; // Return stdin’s write end and the read end of the other two.
 	}
 }}
 
