e802cce99d7c0bc3c2eba365e93cd67ccfd70056
Tuukka Norri <tsnorri@iki.fi>
2022-08-16 15:05:56 +0300

    Add simpler API for SIGCHLD handling

--- include/libbio/dispatch/utility.hh
+++ include/libbio/dispatch/utility.hh
@@ -10,8 +10,17 @@
 
 
 namespace libbio {
+	
+	struct sigchld_handler
+	{
+		virtual ~sigchld_handler() {}
+		virtual void child_did_exit_with_nonzero_status(pid_t const pid, int const exit_status, char const *reason) = 0;
+		virtual void child_received_signal(pid_t const pid, int const signal_number) = 0;
+		virtual void finish_handling(bool const did_report_error) = 0;
+	};
 
 	void install_dispatch_sigchld_handler(dispatch_queue_t queue, dispatch_block_t block);
+	void install_dispatch_sigchld_handler(dispatch_queue_t queue, sigchld_handler &handler);
 }
 
 #endif
