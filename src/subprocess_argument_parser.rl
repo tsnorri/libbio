0b2bbf3c6ccd41ad65bda3da9472c5c31d2326e5
Tuukka Norri <tsnorri@iki.fi>
2022-08-16 15:11:13 +0300

    Add parser for command line parameters
    – The parser splits commands by spaces and also considers quotes.

---
+++ src/subprocess_argument_parser.rl
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2022 Tuukka Norri
+ * This code is licensed under MIT license (see LICENSE for details).
+ */
+
+#include <boost/format.hpp>
+#include <cstring>
+#include <libbio/subprocess.hh>
+
+
+%% machine subprocess_argument_parser;
+%% write data;
+
+
+namespace lb	= libbio;
+
+
+namespace libbio {
+	
+	// Parse plain arguments that do not contain spaces and
+	// double quoted arguments that may contain spaces and escaped double quotes ("").
+	std::vector <std::string> parse_command_arguments(char const * const args)
+	{
+		auto const *p(args);
+		auto const *pe(p + ::strlen(args));
+		auto const *eof(pe);
+		auto const *arg_begin(p);
+		int cs(0);
+		bool should_append{};
+		
+		std::vector <std::string> retval;
+		
+		%%{
+			machine subprocess_argument_parser;
+			
+			action begin_arg {
+				arg_begin = fpc;
+				should_append = false;
+			}
+			
+			action end_plain {
+				std::string_view const sv(arg_begin, fpc);
+				retval.emplace_back(sv);
+			}
+			
+			action end_escaped_quote {
+				// end_quoted gets executed when the first quote is seen.
+				if (should_append)
+					retval.back() += '"';
+				else
+					retval.emplace_back("\"");
+				should_append = true;
+				arg_begin = fpc;
+			}
+			
+			action end_quoted {
+				std::string_view const sv(arg_begin, fpc);
+				if (should_append)
+					retval.back() += sv;
+				else
+					retval.emplace_back(sv);
+				should_append = true; // In case we processed an escaping quote.
+				arg_begin = fpc + 1;
+			}
+			
+			action error {
+				auto msg(boost::str(boost::format("Unepected character %d at position %zu") % (+fc) % (fpc - args)));
+				throw std::invalid_argument(std::move(msg));
+			}
+			
+			quote			= ["];
+			quoted_char		= print - quote;
+			plain_char		= graph - quote;
+			escaped_quote	= ('""') %(end_escaped_quote);
+			quoted_text		= (quoted_char | escaped_quote)+;
+			quoted_argument = quote quoted_text >(begin_arg) %(end_quoted) quote;
+			plain_argument	= (plain_char+) >(begin_arg) %(end_plain);
+			argument		= plain_argument | quoted_argument;
+			argument_string	= (argument (space+ argument)*)?;
+			main			:= argument_string $err(error);
+			
+			write init;
+			write exec;
+		}%%
+		
+		return retval;
+	}
+}
---
+++ tests/subprocess_argument_parser.cc
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2022 Tuukka Norri
+ * This code is licensed under MIT license (see LICENSE for details).
+ */
+
+#include <catch2/catch.hpp>
+#include <libbio/subprocess.hh>
+
+namespace gen	= Catch::Generators;
+namespace lb	= libbio;
+
+
+SCENARIO("Subprocess argument parser can parse simple argument lists", "[subprocess_argument_parser]")
+{
+	GIVEN("An empty command")
+	{
+		std::string const args;
+		std::vector <std::string> const expected;
+		
+		WHEN("the string is parsed")
+		{
+			auto const res(lb::parse_command_arguments(args.data()));
+			
+			THEN("the result matches the expected")
+			{
+				CHECK(expected == res);
+			}
+		}
+	}
+	
+	GIVEN("A simple command")
+	{
+		std::string const args("/path/to/executable --arg1=val1 --arg2=val2 pos1 pos2");
+		std::vector <std::string> const expected{"/path/to/executable", "--arg1=val1", "--arg2=val2", "pos1", "pos2"};
+		
+		WHEN("the string is parsed")
+		{
+			auto const res(lb::parse_command_arguments(args.data()));
+			
+			THEN("the result matches the expected")
+			{
+				CHECK(expected == res);
+			}
+		}
+	}
+}
+
+
+SCENARIO("Subprocess argument parser can parse complex argument lists", "[subprocess_argument_parser]")
+{
+	GIVEN("A command with quoted arguments and escaped quotes")
+	{
+		std::string const args("/path/to/executable \"--arg1=val1\" \"--arg2=\"\"\" \"pos1\" \"\"\"\"");
+		std::vector <std::string> const expected{"/path/to/executable", "--arg1=val1", "--arg2=\"", "pos1", "\""};
+		
+		WHEN("the string is parsed")
+		{
+			auto const res(lb::parse_command_arguments(args.data()));
+			
+			THEN("the result matches the expected")
+			{
+				CHECK(expected == res);
+			}
+		}
+	}
+	
+	GIVEN("A command with quoted arguments and escaped quotes (2)")
+	{
+		std::string const args("/path/to/executable \"--arg1=abc\"\"def\"");
+		std::vector <std::string> const expected{"/path/to/executable", "--arg1=abc\"def"};
+		
+		WHEN("the string is parsed")
+		{
+			auto const res(lb::parse_command_arguments(args.data()));
+			
+			THEN("the result matches the expected")
+			{
+				CHECK(expected == res);
+			}
+		}
+	}
+	
+	GIVEN("A command with quoted arguments and escaped quotes (2)")
+	{
+		std::string const args("/path/to/executable \"\"\"abc\"");
+		std::vector <std::string> const expected{"/path/to/executable", "\"abc"};
+		
+		WHEN("the string is parsed")
+		{
+			auto const res(lb::parse_command_arguments(args.data()));
+			
+			THEN("the result matches the expected")
+			{
+				CHECK(expected == res);
+			}
+		}
+	}
+}
+
+
+SCENARIO("Subprocess argument parser can report errors", "[subprocess_argument_parser]")
+{
+	GIVEN("An invalid command")
+	{
+		std::string const args("/path/to/executable \" pos2");
+		
+		WHEN("the string is parsed")
+		{
+			THEN("an exception with the correct message should be thrown")
+			{
+				REQUIRE_THROWS_WITH(lb::parse_command_arguments(args.data()), "Unepected character 0 at position 26");
+			}
+		}
+	}
+}
