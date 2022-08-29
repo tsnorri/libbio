9f4742e43eebc3b2b473ba3e9d28267378939409
Tuukka Norri <tsnorri@iki.fi>
2022-08-29 18:15:19 +0300

    Add functionality for == in addition to <

--- include/libbio/utility/compare_strings_transparent.hh
+++ include/libbio/utility/compare_strings_transparent.hh
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Tuukka Norri
+ * Copyright (c) 2019-2022 Tuukka Norri
  * This code is licensed under MIT license (see LICENSE for details).
  */
 
@@ -13,18 +13,22 @@
 
 
 namespace libbio { namespace detail {
+	
+	template <typename t_cmp>
 	struct compare_char_span
 	{
 		template <typename t_char, std::size_t t_extent>
-		static inline bool compare(std::string const &lhs, std::span <t_char, t_extent> const &rhs)
+		constexpr static inline bool compare(std::string const &lhs, std::span <t_char, t_extent> const &rhs)
 		{
-			return std::string_view(lhs) < std::string_view(rhs.data(), rhs.size());
+			t_cmp cmp;
+			return cmp(std::string_view(lhs), std::string_view(rhs.data(), rhs.size()));
 		}
 		
 		template <typename t_char, std::size_t t_extent>
-		static inline bool compare(std::span <t_char, t_extent> const &lhs, std::string const &rhs)
+		constexpr static inline bool compare(std::span <t_char, t_extent> const &lhs, std::string const &rhs)
 		{
-			return std::string_view(lhs.data(), lhs.size()) < std::string_view(rhs);
+			t_cmp cmp;
+			return cmp(std::string_view(lhs.data(), lhs.size()), std::string_view(rhs));
 		}
 	};
 }}
@@ -32,76 +36,88 @@ namespace libbio { namespace detail {
 
 namespace libbio {
 	
-	struct compare_strings_transparent
+	template <typename t_cmp>
+	struct compare_strings_transparent_tpl
 	{
 		using is_transparent = std::true_type;
 		
 		// Strings
-		inline bool operator()(std::string const &lhs, std::string const &rhs) const
+		constexpr inline bool operator()(std::string const &lhs, std::string const &rhs) const
 		{
-			return lhs < rhs;
+			t_cmp cmp;
+			return cmp(lhs, rhs);
 		}
 		
 		// String views
-		inline bool operator()(std::string_view const &lhs, std::string const &rhs) const
+		constexpr inline bool operator()(std::string_view const &lhs, std::string const &rhs) const
 		{
-			return lhs < std::string_view(rhs);
+			t_cmp cmp;
+			return cmp(lhs, std::string_view(rhs));
 		}
 		
-		inline bool operator()(std::string const &lhs, std::string_view const &rhs) const
+		constexpr inline bool operator()(std::string const &lhs, std::string_view const &rhs) const
 		{
-			return std::string_view(lhs) < rhs;
+			t_cmp cmp;
+			return cmp(std::string_view(lhs), rhs);
 		}
 		
 		// Character arrays
 		template <std::size_t t_n>
-		inline bool operator()(char const (&lhs)[t_n], std::string const &rhs) const
+		constexpr inline bool operator()(char const (&lhs)[t_n], std::string const &rhs) const
 		{
-			return std::string_view(lhs, t_n) < std::string_view(rhs);
+			t_cmp cmp;
+			return cmp(std::string_view(lhs, t_n), std::string_view(rhs));
 		}
 		
 		template <std::size_t t_n>
-		inline bool operator()(std::string const &lhs, char const (&rhs)[t_n]) const
+		constexpr inline bool operator()(std::string const &lhs, char const (&rhs)[t_n]) const
 		{
-			return std::string_view(lhs) < std::string_view(rhs, t_n);
+			t_cmp cmp;
+			return cmp(std::string_view(lhs), std::string_view(rhs, t_n));
 		}
 		
 		// Span
 		template <std::size_t t_n>
-		inline bool operator()(std::string const &lhs, std::span <char, t_n> const &rhs) const
+		constexpr inline bool operator()(std::string const &lhs, std::span <char, t_n> const &rhs) const
 		{
-			return detail::compare_char_span::compare(lhs, rhs);
+			return detail::compare_char_span <t_cmp>::compare(lhs, rhs);
 		}
 		
 		template <std::size_t t_n>
-		inline bool operator()(std::string const &lhs, std::span <char const, t_n> const &rhs) const
+		constexpr inline bool operator()(std::string const &lhs, std::span <char const, t_n> const &rhs) const
 		{
-			return detail::compare_char_span::compare(lhs, rhs);
+			return detail::compare_char_span <t_cmp>::compare(lhs, rhs);
 		}
 		
 		template <std::size_t t_n>
-		inline bool operator()(std::span <char, t_n> const &lhs, std::string const &rhs) const
+		constexpr inline bool operator()(std::span <char, t_n> const &lhs, std::string const &rhs) const
 		{
-			return detail::compare_char_span::compare(lhs, rhs);
+			return detail::compare_char_span <t_cmp>::compare(lhs, rhs);
 		}
 		
 		template <std::size_t t_n>
-		inline bool operator()(std::span <char const, t_n> const &lhs, std::string const &rhs) const
+		constexpr inline bool operator()(std::span <char const, t_n> const &lhs, std::string const &rhs) const
 		{
-			return detail::compare_char_span::compare(lhs, rhs);
+			return detail::compare_char_span <t_cmp>::compare(lhs, rhs);
 		}
 		
 		// char*
-		inline bool operator()(char const *lhs, std::string const &rhs) const
+		constexpr inline bool operator()(char const *lhs, std::string const &rhs) const
 		{
-			return std::string_view(lhs, std::strlen(lhs)) < std::string_view(rhs);
+			t_cmp cmp;
+			return cmp(std::string_view(lhs, std::strlen(lhs)), std::string_view(rhs));
 		}
 		
-		inline bool operator()(std::string const &lhs, char const *rhs) const
+		constexpr inline bool operator()(std::string const &lhs, char const *rhs) const
 		{
-			return std::string_view(lhs) < std::string_view(rhs, std::strlen(rhs));
+			t_cmp cmp;
+			return cmp(std::string_view(lhs), std::string_view(rhs, std::strlen(rhs)));
 		}
 	};
+	
+	
+	typedef compare_strings_transparent_tpl <std::less <void>>		compare_strings_transparent;
+	typedef compare_strings_transparent_tpl <std::equal_to <void>>	string_equal_to_transparent;
 }
 
 #endif
