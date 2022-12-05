ae150f0bf27e23c0826197f6af3877e5d210862a
Tuukka Norri <tsnorri@iki.fi>
2022-12-05 17:05:03 +0200

    Change namespace

--- include/libbio/tuple/slice.hh
+++ include/libbio/tuple/slice.hh
@@ -3,16 +3,16 @@
  * This code is licensed under MIT license (see LICENSE for details).
  */
 
-#ifndef LIBBIO_UTILITY_TUPLE_SLICE_HH
-#define LIBBIO_UTILITY_TUPLE_SLICE_HH
+#ifndef LIBBIO_TUPLE_SLICE_HH
+#define LIBBIO_TUPLE_SLICE_HH
 
 #include <tuple>
 
 
-namespace libbio {
+namespace libbio::tuples {
 	
 	template <std::size_t t_lhs, std::size_t t_rhs, bool t_should_add_references = false, std::size_t t_i = t_lhs, typename t_tuple>
-	auto tuple_slice(t_tuple &&tuple)
+	auto slice(t_tuple &&tuple)
 	{
 		static_assert(
 			!(t_should_add_references && std::is_rvalue_reference_v <t_tuple>),
@@ -20,7 +20,7 @@ namespace libbio {
 		);
 		
 		if constexpr (t_i < t_lhs)
-			return tuple_slice <t_lhs, t_rhs, t_should_add_references, 1 + t_i>(std::forward <t_tuple>(tuple));
+			return slice <t_lhs, t_rhs, t_should_add_references, 1 + t_i>(std::forward <t_tuple>(tuple));
 		else if constexpr (t_rhs <= t_i)
 			return std::tuple <>{};
 		else
@@ -33,21 +33,21 @@ namespace libbio {
 			>																	output_element_type;
 			return std::tuple_cat(
 				std::tuple <output_element_type>{std::get <t_i>(tuple)}, // std::get returns an rvalue if the input is one.
-				tuple_slice <t_lhs, t_rhs, t_should_add_references, 1 + t_i>(std::forward <t_tuple>(tuple))
+				slice <t_lhs, t_rhs, t_should_add_references, 1 + t_i>(std::forward <t_tuple>(tuple))
 			);
 		}
 	}
 	
 	
 	template <std::size_t t_lhs, bool t_should_add_references = false, typename t_tuple>
-	auto tuple_slice(t_tuple &&tuple)
+	auto slice(t_tuple &&tuple)
 	{
-		return tuple_slice <t_lhs, std::tuple_size_v <t_tuple>, t_should_add_references, t_lhs>(std::forward <t_tuple>(tuple));
+		return slice <t_lhs, std::tuple_size_v <t_tuple>, t_should_add_references, t_lhs>(std::forward <t_tuple>(tuple));
 	}
 	
 	
 	template <typename t_tuple, std::size_t t_lhs, std::size_t t_rhs = std::tuple_size_v <t_tuple>, bool t_should_add_references = false>
-	using tuple_slice_t = std::invoke_result_t <decltype(&tuple_slice <t_lhs, t_rhs, t_should_add_references, t_lhs, t_tuple>), t_tuple>;
+	using slice_t = std::invoke_result_t <decltype(&slice <t_lhs, t_rhs, t_should_add_references, t_lhs, t_tuple>), t_tuple>;
 }
 
 #endif
