721b096264da534b4e3d612a4a9af995463a7b85
Tuukka Norri <tsnorri@iki.fi>
2022-12-16 14:18:40 +0200

    Add prepend_t, append_t, make helper consteval

--- include/libbio/tuple/cat.hh
+++ include/libbio/tuple/cat.hh
@@ -19,7 +19,7 @@ namespace libbio::tuples {
 	template <typename... t_tuples>
 	struct cat_with <std::tuple <t_tuples...>>
 	{
-		static auto helper()
+		consteval static auto helper()
 		{
 			// For some reason using std::declval <t_tuples>() instead of t_tuples{}
 			// causes a static assertion to fail in libstdc++.
@@ -37,13 +37,21 @@ namespace libbio::tuples {
 	};
 	
 	
-	// Join the parameter lists of tuples in the given tuple.
+	// Join the parameter lists of the tuples in the given tuple.
 	template <typename t_tuple>
 	using cat_with_t = typename cat_with <t_tuple>::type;
 	
 	// Join the parameter lists of two or more tuples.
 	template <typename... t_tuples>
 	using cat_t = typename cat <t_tuples...>::type;
+	
+	// Prepend to a tuple.
+	template <typename t_type, typename t_tuple>
+	using prepend_t = cat_t <std::tuple <t_type>, t_tuple>;
+	
+	// Append to a tuple.
+	template <typename t_tuple, typename t_type>
+	using append_t = cat_t <t_tuple, std::tuple <t_type>>;
 }
 
 #endif
