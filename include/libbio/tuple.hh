9257b28fbe3c8a4cacc080096ce0ae01db5c348b
Tuukka Norri <tsnorri@iki.fi>
2022-12-16 14:45:27 +0200

    Add new headers

--- include/libbio/tuple.hh
+++ include/libbio/tuple.hh
@@ -6,9 +6,19 @@
 #ifndef LIBBIO_TUPLE_HH
 #define LIBBIO_TUPLE_HH
 
+#include <libbio/tuple/access.hh>
+#include <libbio/tuple/cat.hh>
 #include <libbio/tuple/erase.hh>
+#include <libbio/tuple/filter.hh>
+#include <libbio/tuple/find.hh>
+#include <libbio/tuple/fold.hh>
+#include <libbio/tuple/for.hh>
+#include <libbio/tuple/group_by.hh>
 #include <libbio/tuple/map.hh>
+#include <libbio/tuple/rank.hh>
+#include <libbio/tuple/reusable_tuple.hh>
 #include <libbio/tuple/slice.hh>
 #include <libbio/tuple/unique.hh>
+#include <libbio/tuple/utility.hh>
 
 #endif
