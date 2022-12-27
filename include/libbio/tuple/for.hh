05b5ad5999891f95d5dbaf51f1d24a1b315d1adb
Tuukka Norri <tsnorri@iki.fi>
2022-12-27 23:10:59 +0200

    Add for_each without tuple instance, make use of size_constant

--- include/libbio/tuple/for.hh
+++ include/libbio/tuple/for.hh
@@ -25,12 +25,22 @@ namespace libbio::tuples {
 	
 	
 	// Apply a function to each of the tuple elements.
+	template <typename t_tuple, std::size_t t_i = 0, typename t_fn>
+	constexpr static inline void for_each(t_fn &&fn)
+	{
+		if constexpr (t_i != std::tuple_size_v <t_tuple>)
+		{
+			fn.template operator() <size_constant <t_i>, std::tuple_element_t <t_i, t_tuple>>;
+			for_each <t_tuple, 1 + t_i>(fn);
+		}
+	}
+	
 	template <std::size_t t_i = 0, typename t_tuple, typename t_fn>
 	constexpr static inline void for_each(t_tuple &&tuple, t_fn &&fn)
 	{
 		if constexpr (t_i != std::tuple_size_v <t_tuple>)
 		{
-			fn.template operator() <std::integral_constant <std::size_t, t_i>>(std::get <t_i>(tuple));
+			fn.template operator() <size_constant <t_i>>(std::get <t_i>(tuple));
 			for_each <1 + t_i>(tuple, fn);
 		}
 	}
