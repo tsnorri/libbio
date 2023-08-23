e5c686675b20cd768324db39e63bec95f529c18a
Tuukka Norri <tsnorri@iki.fi>
2023-08-23 14:30:47 +0300

    Add test for extra header fields

--- tests/fasta_reader.cc
+++ tests/fasta_reader.cc
@@ -148,4 +148,25 @@ SCENARIO("FASTA files can be parsed", "[fasta_reader]")
 			}
 		}
 	}
+	
+	GIVEN("A test file with extra header fields")
+	{
+		libbio::mmap_handle <char> handle;
+		handle.open("test-files/extra-fields.fa");
+		
+		WHEN("the file is parsed")
+		{
+			lb::fasta_reader reader;
+			delegate cb;
+			reader.parse(handle, cb);
+			
+			THEN("the parsed FASTA matches the expected")
+			{
+				auto const expected(handle.to_string_view());
+				auto actual(cb.stream().str());
+				actual.pop_back();
+				REQUIRE(expected == actual);
+			}
+		}
+	}
 }
