e1ba71f18d039db5d3edef2e765f055b4b004116
Tuukka Norri <tsnorri@iki.fi>
2023-10-11 15:59:46 +0300

    Add public base class for updatable_range, make updatable_range final
    – The base class has a pure virtual member function for updating the range; hence it will be easier to use custom range types.

--- include/libbio/generic_parser/iterators.hh
+++ include/libbio/generic_parser/iterators.hh
@@ -11,6 +11,41 @@
 #include <libbio/generic_parser/fwd.hh>
 
 
+namespace libbio::parsing {
+	
+	template <typename t_iterator, typename t_sentinel = t_iterator>
+	struct updatable_range_base
+	{
+		constexpr static inline bool is_contiguous{false}; // Since we need to update the range at times, the range is not necessarily contiguous.
+		constexpr static inline bool is_updatable{true};
+		
+		t_iterator	it;
+		t_sentinel	sentinel;
+		std::size_t	cumulative_length{};
+		
+		template <typename t_iterator_, typename t_sentinel_>
+		updatable_range_base(t_iterator_ &&it_, t_sentinel_ &&sentinel_):
+			it(std::forward <t_iterator>(it_)),
+			sentinel(std::forward <t_sentinel_>(sentinel_))
+		{
+		}
+			
+		virtual ~updatable_range_base() {}
+		virtual bool update() = 0;
+		
+		bool is_at_end() { return it == sentinel && (!update() || it == sentinel); }
+		std::size_t position() const
+		{
+			// If iterator is contiguous, we don’t need to count characters in order to determine the position in the current block.s
+			if constexpr (std::contiguous_iterator <t_iterator>)
+				return cumulative_length - std::distance(it, sentinel);
+			else
+				return cumulative_length + it.pos;
+		}
+	};
+}
+
+
 namespace libbio::parsing::detail {
 
 	template <bool> struct range_position;
@@ -64,7 +99,7 @@ namespace libbio::parsing::detail {
 	
 	
 	// Fwd.
-	template <typename t_iterator, typename t_sentinel, typename t_callback>
+	template <typename t_iterator, typename t_sentinel, typename t_callback, typename t_base = updatable_range_base <t_iterator, t_sentinel>>
 	struct updatable_range;
 	
 	
@@ -169,53 +204,41 @@ namespace libbio::parsing::detail {
 	range(t_iterator &&, t_sentinel &&) -> range <t_iterator, t_sentinel>;
 	
 	
-	template <typename t_iterator, typename t_sentinel, typename t_callback>
-	struct updatable_range
+	template <typename t_iterator, typename t_sentinel, typename t_callback, typename t_base>
+	struct updatable_range final : public t_base
 	{
+		typedef t_base															base_type;
 		typedef t_iterator														iterator;
 		typedef updatable_range_iterator <t_iterator, t_sentinel, t_callback>	updatable_iterator;
 		
-		constexpr static inline bool is_contiguous{std::contiguous_iterator <iterator>};
-		constexpr static inline bool is_updatable{true};
+		constexpr static inline bool is_contiguous{base_type::is_contiguous};
+		constexpr static inline bool is_updatable{base_type::is_updatable};
 		
-		t_iterator	it;
-		t_sentinel	sentinel;
 		t_callback	update_callback;
-		std::size_t	cumulative_length{};
 		
 		template <typename t_iterator_, typename t_sentinel_, typename t_callback_>
 		updatable_range(t_iterator_ &&it_, t_sentinel_ &&sentinel_, t_callback_ &&update_callback_):
-			it(std::forward <t_iterator>(it_)),
-			sentinel(std::forward <t_sentinel_>(sentinel_)),
+			base_type(std::forward <t_iterator>(it_), std::forward <t_sentinel_>(sentinel_)),
 			update_callback(std::forward <t_callback_>(update_callback_))
 		{
 		}
 		
-		bool is_at_end() { return it == sentinel && (!update() || it == sentinel); }
 		auto joining_iterator_pair() { return std::make_tuple(updatable_iterator(*this), updatable_iterator()); }
 		
-		inline bool update()
+		inline bool update() override
 		{
 			// To save a bit of space, we have a special case for contiguous iterators.
 			if constexpr (is_contiguous)
 			{
-				auto const retval(update_callback(it, sentinel));
-				cumulative_length += std::distance(it, sentinel); // Adds zero if updating fails.
+				auto const retval(update_callback(this->it, this->sentinel));
+				this->cumulative_length += std::distance(this->it, this->sentinel); // Adds zero if updating fails.
 				return retval;
 			}
 			else
 			{
-				cumulative_length += it.pos;
-				return update_callback(it, sentinel);
+				this->cumulative_length += this->it.pos;
+				return update_callback(this->it, this->sentinel);
 			}
-		}
-		
-		std::size_t position() const
-		{
-			if constexpr (is_contiguous)
-				return cumulative_length - std::distance(it, sentinel);
-			else
-				return cumulative_length + it.pos;
 		}
 	};
 	
