61ab65fbb50666f51e9d0e0738a04bd41eabe6f4
Tuukka Norri <tsnorri@iki.fi>
2023-10-12 16:53:31 +0300

    Add specialisation of map_t for std::index_sequence, add index_constant_sequence_for

--- include/libbio/tuple/map.hh
+++ include/libbio/tuple/map.hh
@@ -13,7 +13,7 @@
 namespace libbio::tuples {
 	
 	template <typename T, template <typename...> typename>
-	requires is_tuple_v <T>
+	requires (is_tuple_v <T>  || is_index_sequence_v <T>)
 	struct map {};
 	
 	template <typename... t_args, template <typename...> typename t_fn>
@@ -22,10 +22,18 @@ namespace libbio::tuples {
 		typedef std::tuple <t_fn <t_args>...> type;
 	};
 	
+	template <std::size_t... t_idxs, template <typename...> typename t_fn>
+	struct map <std::index_sequence <t_idxs...>, t_fn>
+	{
+		// We use std::integral_constant here b.c. t_fn (likely) needs to be a template
+		// that takes zero or more typenames (not a std::size_t before them).
+		typedef std::tuple <t_fn <std::integral_constant <std::size_t, t_idxs>>...> type;
+	};
 	
-	template <typename t_tuple, template <typename...> typename t_fn>
-	requires is_tuple_v <t_tuple>
-	using map_t = typename map <t_tuple, t_fn>::type;
+	
+	template <typename t_type, template <typename...> typename t_fn>
+	requires (is_tuple_v <t_type> || is_index_sequence_v <t_type>)
+	using map_t = typename map <t_type, t_fn>::type;
 	
 	
 	template <typename T, typename, template <typename...> typename>
@@ -56,6 +64,10 @@ namespace libbio::tuples {
 	template <typename t_tuple_1, typename t_tuple_2, template <typename...> typename t_fn>
 	requires (is_tuple_v <t_tuple_1> && is_tuple_v <t_tuple_2>)
 	using cross_product_t = typename cross_product <t_tuple_1, t_tuple_2, t_fn>::type;
+	
+	
+	template <typename t_type>
+	using index_constant_sequence_for = map_t <std::index_sequence_for <t_type>, std::type_identity_t>;
 }
 
 #endif
--- include/libbio/tuple/utility.hh
+++ include/libbio/tuple/utility.hh
@@ -42,6 +42,16 @@ namespace libbio::tuples {
 	
 	template <typename t_type>
 	constexpr static inline auto const is_tuple_v{is_tuple <t_type>::value};
+	
+	
+	template <typename t_type>
+	struct is_index_sequence : public std::false_type {};
+	
+	template <std::size_t... t_idxs>
+	struct is_index_sequence <std::index_sequence <t_idxs...>> : public std::true_type {};
+	
+	template <typename t_type>
+	constexpr static inline auto const is_index_sequence_v{is_index_sequence <t_type>::value};
 }
 
 #endif
