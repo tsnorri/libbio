a266dd7bdedfefc12c6c87d1c7aafa8e22837e88
Tuukka Norri <tsnorri@iki.fi>
2023-10-20 15:36:08 +0300

    Add helpful comments

--- include/libbio/sam/reader.hh
+++ include/libbio/sam/reader.hh
@@ -21,18 +21,18 @@ namespace libbio::sam {
 		typedef parsing::traits::delimited <parsing::delimiter <'\t'>, parsing::delimiter <'\n'>> parser_trait_type;
 		typedef parsing::parser <
 			parser_trait_type,
-			parsing::fields::text <>,						// QNAME, may be *
-			parsing::fields::integer <std::uint16_t>,		// FLAG
-			parsing::fields::text <>,						// RNAME, may be *
-			parsing::fields::integer <std::uint32_t>,		// POS
-			parsing::fields::integer <std::uint8_t>,		// MAPQ
-			fields::cigar_field,							// CIGAR, may be *
-			parsing::fields::text <>,						// RNEXT, may be * or =
-			parsing::fields::integer <std::uint32_t>,		// PNEXT
-			parsing::fields::integer <std::int32_t>,		// TLEN
-			parsing::fields::character_sequence <char>,		// SEQ, may be *
-			parsing::fields::character_sequence <char>,		// QUAL, may be *
-			fields::optional_field							// Optional fields
+			parsing::fields::text <>,						//  0: QNAME, may be *
+			parsing::fields::integer <std::uint16_t>,		//  1: FLAG
+			parsing::fields::text <>,						//  2: RNAME, may be *
+			parsing::fields::integer <std::uint32_t>,		//  3: POS
+			parsing::fields::integer <std::uint8_t>,		//  4: MAPQ
+			fields::cigar_field,							//  5: CIGAR, may be *
+			parsing::fields::text <>,						//  6: RNEXT, may be * or =
+			parsing::fields::integer <std::uint32_t>,		//  7: PNEXT
+			parsing::fields::integer <std::int32_t>,		//  8: TLEN
+			parsing::fields::character_sequence <char>,		//  9: SEQ, may be *
+			parsing::fields::character_sequence <char>,		// 10: QUAL, may be *
+			fields::optional_field							// 11: Optional fields
 		> parser_type;
 		
 	private:
