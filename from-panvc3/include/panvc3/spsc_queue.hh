ed512bc1c5058909374f7939ea50605f8b7a35ad
Tuukka Norri <tsnorri@iki.fi>
2023-12-07 15:34:21 +0200

    Use GCD conditionally

--- from-panvc3/include/panvc3/spsc_queue.hh
+++ from-panvc3/include/panvc3/spsc_queue.hh
@@ -8,13 +8,39 @@
 
 #include <array>
 #include <atomic>
-#include <dispatch/dispatch.h>
-#include <libbio/dispatch/dispatch_ptr.hh>
 #include <numeric>							// std::iota
 #include <panvc3/utility.hh>				// is_power_of_2()
 
+#if defined(PANVC3_USE_GCD) && PANVC3_USE_GCD
+#	include <dispatch/dispatch.h>
+#	include <libbio/dispatch/dispatch_ptr.hh>
+#else
+#	include <semaphore>
+#endif
+
 
 namespace panvc3 {
+	
+#if defined(PANVC3_USE_GCD) && PANVC3_USE_GCD
+	class dispatch_semaphore_gcd
+	{
+	public:
+		typedef libbio::dispatch_ptr <dispatch_semaphore_t>	dispatch_semaphore_ptr;
+		
+	private:
+		dispatch_semaphore_ptr	m_semaphore_ptr{};
+		
+	public:
+		explicit dispatch_semaphore_gcd(std::uint16_t const size):
+			m_semaphore_ptr(dispatch_semaphore_create(size))
+		{
+		}
+		
+		void acquire() { dispatch_semaphore_wait(*m_semaphore_ptr, DISPATCH_TIME_FOREVER); }
+		void release() { dispatch_semaphore_signal(*m_semaphore_ptr); }
+	};
+#endif
+	
 	
 	template <typename t_value, std::uint16_t t_size>
 	class spsc_queue
@@ -41,18 +67,22 @@ namespace panvc3 {
 		
 		typedef std::array <value_type, t_size>				value_array;
 		typedef std::array <index, t_size>					index_array;
-		typedef libbio::dispatch_ptr <dispatch_semaphore_t>	dispatch_semaphore_ptr;
+#if defined(PANVC3_USE_GCD) && PANVC3_USE_GCD		
+		typedef dispatch_semaphore_gcd						semaphore_type;
+#else
+		typedef std::counting_semaphore <t_size>			semaphore_type;
+#endif
 		
 	protected:
 		value_array				m_values;
 		index_array				m_indices;
-		dispatch_semaphore_ptr	m_semaphore_ptr{};
+		semaphore_type			m_semaphore;
 		size_type				m_read_idx{};		// Used by thread 1.
 		size_type				m_write_idx{};		// Used by thread 2.
 		
 	public:
 		spsc_queue():
-			m_semaphore_ptr(dispatch_semaphore_create(t_size))
+			m_semaphore(t_size)
 		{
 			std::iota(m_indices.begin(), m_indices.end(), 0);
 		}
@@ -69,7 +99,7 @@ namespace panvc3 {
 	auto spsc_queue <t_value, t_size>::pop_index() -> size_type
 	{
 		// Called from thread 1.
-		dispatch_semaphore_wait(*m_semaphore_ptr, DISPATCH_TIME_FOREVER);
+		m_semaphore.acquire();
 
 		size_type const val_idx(m_indices[m_read_idx]);
 		++m_read_idx;
@@ -88,7 +118,7 @@ namespace panvc3 {
 		++m_write_idx;
 		m_write_idx %= t_size;
 
-		dispatch_semaphore_signal(*m_semaphore_ptr);
+		m_semaphore.release();
 	}
 }
 
