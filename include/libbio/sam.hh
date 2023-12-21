5a8b3354affc2d7304614e9a5154890cfdc1a2d6
Tuukka Norri <tsnorri@iki.fi>
2023-12-21 16:56:35 +0200

    Add single header for SAM reader

---
+++ include/libbio/sam.hh
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2023 Tuukka Norri
+ * This code is licensed under MIT license (see LICENSE for details).
+ */
+
+#ifndef LIBBIO_SAM_HH
+#define LIBBIO_SAM_HH
+
+#include <libbio/sam/cigar.hh>
+#include <libbio/sam/flag.hh>
+#include <libbio/sam/header.hh>
+#include <libbio/sam/input_range.hh>
+#include <libbio/sam/literals.hh>
+#include <libbio/sam/optional_field.hh>
+#include <libbio/sam/parse_error.hh>
+#include <libbio/sam/reader.hh>
+#include <libbio/sam/record.hh>
+#include <libbio/sam/tag.hh>
+#include <libbio/sam/tag_definitions.hh>
+
+#endif
