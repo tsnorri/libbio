decc1c12dd15d729170bdf36b1fb1f9da0eec46b
Tuukka Norri <tsnorri@iki.fi>
2024-09-03 19:43:18 +0300

    Add missing headers

---
+++ from-panvc3/include/panvc3/dispatch/events/platform/manager_kqueue.hh
@@ -0,0 +1,120 @@
+/*
+ * Copyright (c) 2024 Tuukka Norri
+ * This code is licensed under MIT license (see LICENSE for details).
+ */
+
+#ifndef PANVC3_DISPATCH_EVENTS_PLATFORM_MANAGER_KQUEUE_HH
+#define PANVC3_DISPATCH_EVENTS_PLATFORM_MANAGER_KQUEUE_HH
+
+#include <panvc3/dispatch/events/manager.hh>
+#include <signal.h>								// ::sigemptyset
+
+
+namespace panvc3::dispatch::events::platform::kqueue {
+	
+	typedef std::int16_t	filter_type;				// Type from struct kevent.
+	
+	
+	struct source_key
+	{
+		int			value{};
+		filter_type	filter{};
+		
+		source_key() = default;
+		
+		constexpr source_key(int const value_, filter_type const filter_):
+			value(value_),
+			filter(filter_)
+		{
+		}
+		
+		constexpr inline bool operator==(source_key const other) const { return value == other.value && filter == other.filter; }
+	};
+}
+
+
+template <>
+struct std::hash <panvc3::dispatch::events::platform::kqueue::source_key>
+{
+	constexpr std::size_t operator()(panvc3::dispatch::events::platform::kqueue::source_key const &sk) const noexcept
+	{
+		std::size_t retval(std::abs(sk.value));
+		retval <<= 17;
+		retval |= std::size_t(sk.value < 0 ? 0x1 : 0x0) << 16;
+		retval |= std::bit_cast <std::uint16_t>(sk.filter);
+		return retval;
+	}
+};
+
+
+namespace panvc3::dispatch::events::platform::kqueue {
+
+	class signal_monitor
+	{
+		sigset_t	m_mask{};
+		sigset_t	m_original_mask{};
+		bool		m_is_empty{true};
+		
+	public:
+		signal_monitor()
+		{
+			sigemptyset(&m_mask);
+			sigemptyset(&m_original_mask);
+		}
+		
+		void listen(int sig);
+		void unlisten(int sig); // Returns the old file descriptor.
+	};
+
+
+	class manager final : public events::manager_base
+	{
+		typedef events::detail::file_handle		file_handle;
+		
+		typedef std::unordered_multimap <
+			source_key,
+			std::shared_ptr <events::source>
+		>										source_map;
+		
+		file_handle								m_kqueue_handle;
+		source_map								m_sources;
+		signal_monitor							m_signal_monitor;
+		
+		std::mutex								m_mutex{};
+		
+	public:
+		void setup() override;
+		void run() override;
+		
+		file_descriptor_source &add_file_descriptor_read_event_source(
+			file_descriptor_type const fd,
+			queue &qq,
+			file_descriptor_source::task_type tt
+		) override;									// Thread-safe.
+		
+		file_descriptor_source &add_file_descriptor_write_event_source(
+			file_descriptor_type const fd,
+			queue &qq,
+			file_descriptor_source::task_type tt
+		) override;									// Thread-safe.
+		
+		signal_source &add_signal_event_source(
+			signal_type const sig,
+			queue &qq,
+			signal_source::task_type tt
+		) override;									// Thread-safe.
+		
+		void remove_file_descriptor_event_source(
+			file_descriptor_source &es
+		) override;									// Thread-safe.
+		
+		void remove_signal_event_source(
+			signal_source &es
+		) override;									// Thread-safe.
+		
+	private:
+		void remove_event_source(source &es, source_key const key);
+	};
+}
+
+#endif
---
+++ from-panvc3/include/panvc3/dispatch/events/platform/manager_linux.hh
@@ -0,0 +1,195 @@
+/*
+ * Copyright (c) 2024 Tuukka Norri
+ * This code is licensed under MIT license (see LICENSE for details).
+ */
+
+#ifndef PANVC3_DISPATCH_EVENTS_PLATFORM_MANAGER_LINUX_HH
+#define PANVC3_DISPATCH_EVENTS_PLATFORM_MANAGER_LINUX_HH
+
+#include <panvc3/dispatch/events/manager.hh>
+#include <signal.h>
+#include <sys/signalfd.h>	// struct signalfd_siginfo
+
+
+namespace panvc3::dispatch::events::platform::linux {
+	
+	struct source_key
+	{
+		struct fd_tag {};
+		struct signal_tag {};
+		
+		int value{}; // Both signals and file descriptors have type int in POSIX.
+		bool is_signal{};
+		
+		source_key() = default;
+		
+		constexpr source_key(file_descriptor_type const fd, fd_tag):
+			vallue(fd)
+		{
+		}
+		
+		constexpr source_key(signal_type const sig, signal_tag):
+			value(sig),
+			is_signal(true)
+		{
+		}
+		
+		constexpr inline bool operator==(source_key const other) const
+	};
+}
+
+
+template <>
+std::hash <panvc3::dispatch::events::platform::linux::source_key>
+{
+	constexpr std::size_t operator()(panvc3::dispatch::events::platform::linux::source_key const &sk) const noexcept
+	{
+		std::size_t retval{std::abs(sk.value)};
+		retval <<= 2;
+		retval |= std::size_t(sk.value < 0 ? 0x1 : 0x0) << 1;
+		retval |= sk.is_signal;
+		return retval;
+	}
+};
+
+
+namespace panvc3::dispatch::events::platform::linux {
+	
+	struct fd_counter
+	{
+		std::uint8_t	reader_count{};
+		std::uint8_t	writer_count{};
+		
+		constexpr operator bool() const { return reader_count || writer_count; }
+	};
+	
+	
+	struct timer
+	{
+		file_handle	handle;
+
+		void prepare();
+		void start(struct timespec const ts);
+		void release() { handle.release(); }
+	};
+
+
+	class signal_monitor
+	{
+		file_handle m_handle;
+		sigset_t m_mask{};
+		sigset_t m_original_mask{};
+		
+		signal_monitor()
+		{
+			::sigemptyset(&m_mask);
+			::sigemptyset(&m_original_mask);
+		}
+		
+		bool listen(int sig);
+		int unlisten(int sig); // Returns the old file descriptor.
+		bool read(struct signalfd_siginfo &buffer);
+		void release();
+	};
+
+
+	class event_monitor
+	{
+		file_handle							m_handle;
+		std::vector <manager::event_type>	m_events;
+		std::mutex							m_mutex;
+
+	public:
+		void prepare();
+
+		int file_descriptor() const { return m_handle.fd; }
+		std::lock_guard lock() { return {m_mutex}; }
+		void post(manager::event_type const event);
+		std::vector <manager::event_type> const &events() const { return m_events; }
+		void clear();
+	};
+	
+	
+	class manager final : public events::manager_base
+	{
+	private:
+		typedef events::detail::source_key		source_key;
+		typedef events::detail::fd_counter		fd_counter;
+		
+		typedef std::unordered_multimap <
+			source_key,
+			std::shared_ptr <events::source>
+		>										source_map;
+		
+		typedef std::unordered_map <
+			file_descriptor_type,
+			fd_counter
+		>										counter_map;
+		
+	private:
+		file_handle								m_epoll_handle;
+		
+		source_map								m_sources;
+		counter_map								m_reader_writer_counts;
+		
+		signal_monitor							m_signal_monitor;
+		event_monitor							m_event_monitor;
+		timer									m_timer;
+		
+		std::mutex								m_mutex{};
+		
+	public:
+		void setup() override;
+		void run() override;
+		
+		file_descriptor_source &add_file_descriptor_read_event_source(
+			file_descriptor_type const fd,
+			queue &qq,
+			file_descriptor_source::task_type tt
+		) override;									// Thread-safe.
+		
+		file_descriptor_source &add_file_descriptor_write_event_source(
+			file_descriptor_type const fd,
+			queue &qq,
+			file_descriptor_source::task_type tt
+		) override;									// Thread-safe.
+		
+		signal_source &add_signal_event_source(
+			signal_type const sig,
+			queue &qq,
+			signal_source::task_type tt
+		) override;									// Thread-safe.
+		
+		void remove_file_descriptor_event_source(
+			file_descriptor_source &es
+		) override;									// Thread-safe.
+		
+		void remove_signal_event_source(
+			signal_source &es
+		) override;									// Thread-safe.
+		
+	private:
+		void listen_for_fd_events(int fd, bool for_read, bool for_write);
+		inline void listen_for_fd_events(file_descriptor_event_source const &source);
+		void schedule_kernel_timer(duration_type const dur);
+	};
+	
+	
+	void manager::listen_for_fd_events(file_descriptor_event_source const &source)
+	{
+		listen_for_fd_events(
+			source.file_descriptor(),
+			source.is_read_event_source(),
+			source.is_write_event_source()
+		);
+	}
+	
+	
+	bool source_key::operator==(source_key const other) const
+	{
+		if (is_signal != other.is_signal)
+			return false;
+		
+		return value == other.value;
+	}
+}
