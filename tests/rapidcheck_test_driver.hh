b060bc86064e7419eb06aeba43fdd5436c7fbe7e
Tuukka Norri <tsnorri@iki.fi>
2024-10-07 16:07:00 +0300

    Add tests for gte_power_of_2, support for TEMPLATE_TEST_CASE in RapidCheck test driver

--- tests/rapidcheck_test_driver.hh
+++ tests/rapidcheck_test_driver.hh
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Tuukka Norri
+ * Copyright (c) 2023-2024 Tuukka Norri
  * This code is licensed under MIT license (see LICENSE for details).
  */
 
@@ -7,8 +7,11 @@
 #ifndef LIBBIO_RAPIDCHECK_TEST_DRIVER
 #define LIBBIO_RAPIDCHECK_TEST_DRIVER
 
+#include <boost/type_index.hpp>								// boost::typeindex::type_id <t_type>().pretty_name()
 #include <exception>										// Needed by RapidCheck
+#include <format>
 #include <iostream>
+#include <libbio/tuple/map.hh>
 #include <libbio/utility/compare_strings_transparent.hh>
 #include <rapidcheck.h>
 #include <set>
@@ -19,9 +22,10 @@
 
 #ifdef BUILD_RAPIDCHECK_TEST_DRIVER
 
-namespace libbio {
+namespace libbio::tests {
 
 	struct test_case;
+	struct template_test_case;
 	
 	
 	class test_driver
@@ -33,7 +37,8 @@ namespace libbio {
 		> test_name_set;
 
 	private:
-		std::vector <test_case *> m_test_cases;
+		std::vector <test_case *>			m_test_cases;
+		std::vector <template_test_case *>	m_template_test_cases;
 		
 	public:
 		static test_driver &shared()
@@ -48,33 +53,58 @@ namespace libbio {
 			return m_test_cases.size() - 1;
 		}
 		
+		std::size_t add_template_test_case(template_test_case &tc)
+		{
+			m_template_test_cases.emplace_back(&tc);
+			return m_template_test_cases.size() - 1;
+		}
+		
 		void list_tests();
+		void list_template_tests();
 		std::size_t run_all_tests();
 		std::size_t run_given_tests(test_name_set const &names);
+		std::size_t run_given_template_tests(test_name_set const &names);
 	};
 	
 	
-	struct test_case
+	struct test_case_base
 	{
 		std::size_t	identifier{};
 		
-		test_case()
-		{
-			identifier = test_driver::shared().add_test_case(*this);
-		}
-		
-		virtual ~test_case() {}
+		virtual ~test_case_base() {}
 		virtual char const *message() = 0;
 		virtual bool run_test() = 0;
 	};
 	
 	
-	template <typename ... t_args>
+	struct test_case : public test_case_base
+	{
+		test_case()
+		{
+			identifier = test_driver::shared().add_test_case(*this);
+		}
+	};
+	
+	
+	struct template_test_case : public test_case_base
+	{
+		template_test_case()
+		{
+			identifier = test_driver::shared().add_template_test_case(*this);
+		}
+	};
+}
+	
+
+namespace libbio {
+	
+	template <typename... t_args>
 	auto rc_check(t_args && ... args)
 	{
 		return rc::check(std::forward <t_args>(args)...);
 	}
 }
+
 
 #	define LIBBIO_CONCAT_TOKEN_2(lhs, rhs) lhs ## rhs
 #	define LIBBIO_CONCAT_TOKEN(lhs, rhs) LIBBIO_CONCAT_TOKEN_2(lhs, rhs)
@@ -84,14 +114,38 @@ namespace libbio {
 // It is very important that all of the following are on the same line.
 #	define TEST_CASE(MESSAGE, TAGS) \
 		static inline bool __attribute__ ((always_inline)) LIBBIO_TEST_FN_NAME(); /* Forward declaration */ \
-		static struct : public ::libbio::test_case { \
+		static struct : public ::libbio::tests::test_case { \
 			char const *message() override { return MESSAGE; } \
 			bool run_test() override { return LIBBIO_TEST_FN_NAME(); } \
 		} LIBBIO_TEST_CASE_VARIABLE_NAME; \
 		static inline bool __attribute__ ((always_inline)) LIBBIO_TEST_FN_NAME()
-	
+
+// Also here.
+#	define TEMPLATE_TEST_CASE(MESSAGE, TAGS, ...) \
+		template <typename TestType> \
+		static bool LIBBIO_TEST_FN_NAME(); /* Forward declaration */ \
+		static struct : public ::libbio::tests::test_case { \
+			template <typename t_type> \
+			class test_case final : public ::libbio::tests::template_test_case { \
+			private: \
+				std::string m_message; \
+			public: \
+				test_case(): \
+					m_message{std::format("{} [{}]", MESSAGE, boost::typeindex::type_id <t_type>().pretty_name())} \
+				{ \
+				} \
+				char const *message() override { return m_message.data(); } \
+				bool run_test() override { return LIBBIO_TEST_FN_NAME <t_type>(); } \
+			}; \
+			libbio::tuples::map_t <std::tuple <__VA_ARGS__>, test_case> tests{}; \
+			char const *message() override { return MESSAGE; } \
+			bool run_test() override { return std::apply([](auto... tt){ return (tt.run_test() && ...); }, tests); } \
+		} LIBBIO_TEST_CASE_VARIABLE_NAME; \
+		template <typename TestType> \
+		static bool LIBBIO_TEST_FN_NAME()
 #else
 #	include <catch2/catch_test_macros.hpp>
+#	include <catch2/catch_template_test_macros.hpp>
 #	include <rapidcheck/catch.h>	// rc::prop
 
 namespace libbio {
--- tests/rapidcheck_test_driver_main.cc
+++ tests/rapidcheck_test_driver_main.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Tuukka Norri
+ * Copyright (c) 2023-2024 Tuukka Norri
  * This code is licensed under MIT license (see LICENSE for details).
  */
 
@@ -14,7 +14,7 @@ namespace lb	= libbio;
 
 namespace {
 
-	bool run_test(::libbio::test_case &tc)
+	bool run_test(::libbio::tests::test_case_base &tc)
 	{
 		std::cerr << "* Running test: " << tc.message() << '\n';
 		return tc.run_test();
@@ -22,14 +22,20 @@ namespace {
 }
 
 
-namespace libbio {
+namespace libbio::tests {
 
 	void test_driver::list_tests()
 	{
 		for (auto ptr : m_test_cases)
 			std::cout << ptr->message() << '\n';
 	}
-
+	
+	
+	void test_driver::list_template_tests()
+	{
+		for (auto ptr : m_template_test_cases)
+			std::cout << ptr->message() << '\n';
+	}
 	
 	std::size_t test_driver::run_all_tests()
 	{
@@ -54,6 +60,19 @@ namespace libbio {
 
 		return retval;
 	}
+	
+	
+	std::size_t test_driver::run_given_template_tests(test_name_set const &names)
+	{
+		std::size_t retval{};
+		for (auto &ptr : m_template_test_cases)
+		{
+			if (names.contains(ptr->message()) && !run_test(*ptr))
+				++retval;
+		}
+
+		return retval;
+	}
 }
 
 
@@ -63,22 +82,41 @@ int main(int argc, char **argv)
 	if (0 != cmdline_parser (argc, argv, &args_info))
 		std::exit(EXIT_FAILURE);
 
-	auto &driver(::libbio::test_driver::shared());
+	auto &driver(::libbio::tests::test_driver::shared());
 
 	if (args_info.list_given)
 	{
 		driver.list_tests();
 		return EXIT_SUCCESS;
 	}
-
-	std::size_t status{};
-	if (0 == args_info.test_given)
-		status = driver.run_all_tests();
-	else
+	
+	if (args_info.list_templates_given)
 	{
-		::libbio::test_driver::test_name_set const test_names(args_info.test_arg, args_info.test_arg + args_info.test_given);
-		status = driver.run_given_tests(test_names);
+		driver.list_template_tests();
+		return EXIT_SUCCESS;
 	}
 
+	std::size_t status{};
+	
+	if (args_info.test_given)
+	{
+		::libbio::tests::test_driver::test_name_set const test_names(args_info.test_arg, args_info.test_arg + args_info.test_given);
+		status = driver.run_given_tests(test_names);
+		if (status)
+			goto bail;
+	}
+	
+	if (args_info.template_test_given)
+	{
+		::libbio::tests::test_driver::test_name_set const test_names(args_info.template_test_arg, args_info.template_test_arg + args_info.template_test_given);
+		status = driver.run_given_template_tests(test_names);
+		if (status)
+			goto bail;
+	}
+	
+	if (! (args_info.test_given || args_info.template_test_given))
+		status = driver.run_all_tests();
+
+bail:
 	return status ? EXIT_FAILURE : EXIT_SUCCESS;
 }
