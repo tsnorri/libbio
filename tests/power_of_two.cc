1af148c451c81a6fc4ed38ebbe63942906dcbd62
Tuukka Norri <tsnorri@iki.fi>
2024-10-10 11:23:04 +0300

    Add tests for bits::gte_power_of_2

---
+++ tests/power_of_two.cc
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2024 Tuukka Norri
+ * This code is licensed under MIT license (see LICENSE for details).
+ */
+
+#include <catch2/catch_template_test_macros.hpp>
+#include <libbio/bits.hh>
+
+namespace lb		= libbio;
+
+
+TEMPLATE_TEST_CASE(
+	"bits::gte_power_of_2 works with simple valid inputs",
+	"[gte_power_of_2]",
+	std::uint8_t,
+	std::uint16_t,
+	std::uint32_t,
+	std::uint64_t
+)
+{
+	GIVEN("Value 1 of a tested type")
+	{
+		TestType const value(1);
+		auto const res(lb::bits::gte_power_of_2(value));
+		CHECK(TestType(1) == res);
+	}
+}
---
+++ tests/power_of_two_arbitrary.cc
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2024 Tuukka Norri
+ * This code is licensed under MIT license (see LICENSE for details).
+ */
+
+#include <libbio/bits.hh>
+#include "rapidcheck_test_driver.hh"
+
+namespace lb		= libbio;
+
+namespace {
+	
+	template <typename t_type>
+	struct input
+	{
+		typedef t_type type;
+		t_type value{};
+		
+		constexpr t_type get() const { return value; }
+		constexpr operator t_type() const { return value; }
+	};
+	
+	
+	template <typename t_type>
+	struct valid_input : public input <t_type> {};
+	
+	template <typename t_type>
+	struct invalid_input : public input <t_type> {};
+	
+	
+	template <typename t_type>
+	constexpr t_type highest_bit_set()
+	{
+		return t_type(1) << (sizeof(t_type) * CHAR_BIT - 1);
+	}
+}
+
+
+namespace rc {
+	
+	template <typename t_type>
+	struct Arbitrary <valid_input <t_type>>
+	{
+		static Gen <valid_input <t_type>> arbitrary()
+		{
+			return gen::construct <valid_input <t_type>>(
+				gen::inRange(t_type(0), highest_bit_set <t_type>())
+			);
+		}
+	};
+	
+	
+	template <typename t_type>
+	struct Arbitrary <invalid_input <t_type>>
+	{
+		static Gen <invalid_input <t_type>> arbitrary()
+		{
+			return gen::construct <invalid_input <t_type>>(
+				gen::inRange(t_type(highest_bit_set <t_type>() + 1), std::numeric_limits <t_type>::max())
+			);
+		}
+	};
+}
+
+
+TEMPLATE_TEST_CASE(
+	"bits::gte_power_of_2 works with arbitrary valid input",
+	"[template][gte_power_of_2]",
+	valid_input <std::uint8_t>,
+	valid_input <std::uint16_t>,
+	valid_input <std::uint32_t>,
+	valid_input <std::uint64_t>
+)
+{
+	return lb::rc_check(
+		"bits::is_power_of_2 works with arbitrary valid input",
+		[](TestType const value){
+			auto const value_(value.get());
+			auto const power(lb::bits::gte_power_of_2(value_));
+			RC_ASSERT(power);
+			auto const power_(*power);
+			RC_ASSERT(value_ <= power_);
+			RC_ASSERT(lb::bits::is_power_of_2(power_));
+			RC_CLASSIFY(lb::bits::is_power_of_2(value_));
+			return true;
+		}
+	);
+}
+
+
+TEMPLATE_TEST_CASE(
+	"bits::gte_power_of_2 works with arbitrary invalid input",
+	"[template][gte_power_of_2]",
+	invalid_input <std::uint8_t>,
+	invalid_input <std::uint16_t>,
+	invalid_input <std::uint32_t>,
+	invalid_input <std::uint64_t>
+)
+{
+	return lb::rc_check(
+		"bits::is_power_of_2 works with arbitrary invalid input",
+		[](TestType const value){
+			auto const value_(value.get());
+			auto const power(lb::bits::gte_power_of_2(value_));
+			RC_ASSERT(!power);
+			return true;
+		}
+	);
+}
