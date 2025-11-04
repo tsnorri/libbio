01103c832b59e0890c5dae4c5611b877795ba306
Tuukka Norri <tsnorri@iki.fi>
2025-11-04 15:19:54 +0200

    Fix maintaining input buffer state

--- src/gzip_read_handle.cc
+++ src/gzip_read_handle.cc
@@ -4,6 +4,7 @@
  */
 
 #include <cstddef>
+#include <iostream>
 #include <libbio/assert.hh>
 #include <libbio/file_handle.hh>
 #include <libbio/gzip_read_handle.hh>
@@ -29,7 +30,7 @@ namespace libbio {
 		if (m_input_buffer.size() < m_io_op_blocksize)
 		{
 			// Make sure that at least two blocks fit into the buffer.
-			auto const page_count((3 * m_io_op_blocksize - 1) / m_input_buffer.page_size());
+			auto const page_count((3 * m_io_op_blocksize - 1) / m_input_buffer.page_size() ?: 1);
 			m_input_buffer.allocate(page_count);
 		}
 		m_input_buffer.clear();
@@ -58,7 +59,7 @@ namespace libbio {
 		m_stream.next_out = reinterpret_cast <unsigned char *>(dst);
 		while (true)
 		{
-			auto prev_avail_in{m_stream.avail_in};
+			auto const * const prev_input_pos{m_stream.next_in};
 			while (m_stream.avail_in && m_stream.avail_out)
 			{
 				auto const res{::inflate(&m_stream, Z_SYNC_FLUSH)};
@@ -66,6 +67,11 @@ namespace libbio {
 				{
 					case Z_OK:
 					{
+						// Update the input buffer state.
+						auto const processed_input_bytes{m_stream.next_in - prev_input_pos};
+						m_input_buffer.add_to_available(processed_input_bytes);
+
+						// Check how much data were inflated.
 						auto const read_amount{requested_length - m_stream.avail_out};
 						if (read_amount != prev_read_amount && m_stream.avail_out)
 						{
@@ -82,6 +88,11 @@ namespace libbio {
 
 					case Z_STREAM_END:
 					{
+						// Update the input buffer state.
+						auto const processed_input_bytes{m_stream.next_in - prev_input_pos};
+						m_input_buffer.add_to_available(processed_input_bytes);
+
+						// Check how much data were inflated.
 						auto const read_amount{requested_length - m_stream.avail_out};
 						return read_amount;
 					}
@@ -97,11 +108,6 @@ namespace libbio {
 						throw std::runtime_error("Problem with inflate");
 				}
 
-				// Update the input buffer state.
-				auto const processed_input_bytes{prev_avail_in - m_stream.avail_in};
-				m_input_buffer.add_to_available(processed_input_bytes);
-				prev_avail_in = m_stream.avail_in;
-
 				// Update the reading position.
 				auto reading_range{m_input_buffer.reading_range_()};
 				m_stream.avail_in = reading_range.size();
@@ -114,6 +120,14 @@ namespace libbio {
 			auto writing_range{m_input_buffer.writing_range()};
 			auto const read_size{m_gzip_handle->read(available_size, writing_range.it)};
 			m_input_buffer.add_to_occupied(read_size);
+
+			// Update the reading position.
+			auto reading_range{m_input_buffer.reading_range_()};
+			m_stream.avail_in = reading_range.size();
+			m_stream.next_in = reinterpret_cast <unsigned char *>(reading_range.it);
+
+			if (0 == m_stream.avail_in)
+				return 0;
 		}
 	}
 }
