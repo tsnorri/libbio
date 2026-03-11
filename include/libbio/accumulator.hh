e2257468a462a176c5ddd0fb081fe232f0de8a6d
Tuukka Norri <tsnorri@iki.fi>
2026-03-11 12:34:26 +0200

    Include headers properly

--- include/libbio/accumulator.hh
+++ include/libbio/accumulator.hh
@@ -6,6 +6,8 @@
 #ifndef LIBBIO_ACCUMULATOR_HH
 #define LIBBIO_ACCUMULATOR_HH
 
+#include <libbio/accumulator/mean.hh>						// IWYU pragma: export
+#include <libbio/accumulator/min_max.hh>					// IWYU pragma: export
 #include <libbio/accumulator/pearson_correlation.hh>		// IWYU pragma: export
 
 #endif
