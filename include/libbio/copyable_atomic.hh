2f0d02fc0ed6aeb108a8c68b8bf11e150fede807
Tuukka Norri <tsnorri@iki.fi>
2026-03-23 17:17:35 +0200

    Make member functions public

--- include/libbio/copyable_atomic.hh
+++ include/libbio/copyable_atomic.hh
@@ -10,23 +10,39 @@
 
 
 namespace libbio {
-	
+
 	template <typename t_value>
 	class copyable_atomic final : public std::atomic <t_value>
 	{
 	public:
-		using std::atomic <t_value>::atomic;
-		
+		typedef std::atomic <t_value> base_type;
+
+		using base_type::atomic;
+
+		using base_type::load;
+		using base_type::store;
+
+		using base_type::fetch_add;
+		using base_type::fetch_sub;
+		using base_type::fetch_and;
+		using base_type::fetch_or;
+		using base_type::fetch_xor;
+
+#if defined(__cpp_lib_atomic_min_max) and 202403L <= __cpp_lib_atomic_min_max
+		using base_type::fetch_min;
+		using base_type::fetch_max;
+#endif
+
 		copyable_atomic():
 			std::atomic <t_value>()
 		{
 		}
-		
+
 		copyable_atomic(copyable_atomic const &other)
 		{
 			this->store(other.load(std::memory_order_acquire), std::memory_order_release);
 		}
-		
+
 		copyable_atomic &operator=(copyable_atomic const &other) &
 		{
 			if (this != &other)
